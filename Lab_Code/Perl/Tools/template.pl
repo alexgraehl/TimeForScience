d4bee1e7b93ebc5a2f8e09ff213e077768a5e155
alexgw <alexgw@localhost>
2011-01-11 11:23:20 -0800

    Changes committed by alexgw

--- Lab_Code/Perl/Tools/template.pl
+++ Lab_Code/Perl/Tools/template.pl
@@ -1,9 +1,9 @@
 #!/usr/bin/perl
 
-use lib "$ENV{MYPERLDIR}/lib"; use lib "$ENV{TIME_FOR_SCIENCE_DIR}/Lab_Code/Perl/LabLibraries"; require "libfile.pl";
-use lib "$ENV{MYPERLDIR}/lib"; use lib "$ENV{TIME_FOR_SCIENCE_DIR}/Lab_Code/Perl/LabLibraries"; require "libstats.pl";
-use lib "$ENV{MYPERLDIR}/lib"; use lib "$ENV{TIME_FOR_SCIENCE_DIR}/Lab_Code/Perl/LabLibraries"; require "libstring.pl";
-use lib "$ENV{MYPERLDIR}/lib"; use lib "$ENV{TIME_FOR_SCIENCE_DIR}/Lab_Code/Perl/LabLibraries"; require "libsystem.pl";
+#use lib "$ENV{MYPERLDIR}/lib"; use lib "$ENV{TIME_FOR_SCIENCE_DIR}/Lab_Code/Perl/LabLibraries"; require "libfile.pl";
+#use lib "$ENV{MYPERLDIR}/lib"; use lib "$ENV{TIME_FOR_SCIENCE_DIR}/Lab_Code/Perl/LabLibraries"; require "libstats.pl";
+#use lib "$ENV{MYPERLDIR}/lib"; use lib "$ENV{TIME_FOR_SCIENCE_DIR}/Lab_Code/Perl/LabLibraries"; require "libstring.pl";
+#use lib "$ENV{MYPERLDIR}/lib"; use lib "$ENV{TIME_FOR_SCIENCE_DIR}/Lab_Code/Perl/LabLibraries"; require "libsystem.pl";
 
 #use List::Util 'shuffle';
 #@shuffled = shuffle(@list);
@@ -27,6 +27,21 @@ sub main();
 #print "Arr";
 #colorResetString();
 
+sub colorString($) {
+  # Requires: use Term::AnsiColor at the top of your file. Note: you have to
+  # PRINT the result of this function! Only sets the output color if the output
+  # is a TERMINAL. If the output is NOT a terminal, then colorizing output
+  # results in lots of garbage characters (the color control characters)
+  # written to the screen.
+  my ($theColor) = @_;
+  if (-t STDOUT) { # <-- checks to see if STDOUT goes directly to the terminal (instead of, say, outputting to a file with a redirect)
+	return color($theColor);
+  }
+}
+
+sub quitWithUsageError($) { print($_[0] . "\n"); printUsage(); print($_[0] . "\n"); }
+sub printUsageAndQuit() { printUsage(); exit(1); }
+
 sub printUsage() {
     print STDOUT <DATA>;
     exit(0);
@@ -34,42 +49,48 @@ sub printUsage() {
 
 # ==1==
 sub main() { # Main program
-	my ($delim) = "\t";
-	my ($decimalPlaces) = 4; # How many decimal places to print, by default
-	$Getopt::Long::passthrough = 1; # ignore arguments we don't recognize in GetOptions, and put them in @ARGV
+    my ($delim) = "\t";
+    my ($decimalPlaces) = 4; # How many decimal places to print, by default
+    $Getopt::Long::passthrough = 1; # ignore arguments we don't recognize in GetOptions, and put them in @ARGV
 
-	GetOptions("help|?|man" => sub { printUsageAndQuit(); }
+    GetOptions("help|?|man" => sub { printUsageAndQuit(); }
 	       , "delim|d=s" => \$delim
-		   , "dp=i" => \$decimalPlaces
-	       ) or printUsageAndQuit();
+	       , "dp=i" => \$decimalPlaces
+	) or printUsageAndQuit();
 
-	if (1 == 0) {
-	  quitWithUsageError("1 == 0? Something is wrong!");
+    print STDERR colorString("green");
+    print STDERR "===============================\n";
+    print STDERR "Test color!\n";
+    print STDERR "===============================\n";
+    print STDERR colorString("reset");
+
+    if (1 == 0) {
+	quitWithUsageError("1 == 0? Something is wrong!");
+    }
+
+    my $numUnprocessedArgs = scalar(@ARGV);
+    if ($numUnprocessedArgs != 2) {
+	quitWithUsageError("Error in arguments! You must send TWO filenames to this program.\n");
+    }
+
+    my $filename1 = undef;
+    my $filename2 = undef;
+
+    foreach (@ARGV) { # these were arguments that were not understood by GetOptions
+	if (!defined($filename1)) { $filename1 = $_; }
+	elsif (!defined($filename2)) { $filename2 = $_; }
+	else {
+	    print STDERR "Unprocessed argument: $_\n";
 	}
+    }
 
-	my $numUnprocessedArgs = scalar(@ARGV);
-	if ($numUnprocessedArgs != 2) {
-	  quitWithUsageError("Error in arguments! You must send TWO filenames to this program.\n");
-	}
+    #print "\t" . sprintf("%.${numDecimalPointsToPrint}f", $levFraction);
+    #my @col1 = @{readFileColumn($filename1, 0, $delim)};
 
-	my $filename1 = undef;
-	my $filename2 = undef;
-
-	foreach (@ARGV) { # these were arguments that were not understood by GetOptions
-	  if (!defined($filename1)) { $filename1 = $_; }
-	  elsif (!defined($filename2)) { $filename2 = $_; }
-	  else {
-	      print STDERR "Unprocessed argument: $_\n";
-	  }
-	}
-
-	#print "\t" . sprintf("%.${numDecimalPointsToPrint}f", $levFraction);
-	#my @col1 = @{readFileColumn($filename1, 0, $delim)};
-
-	print STDERR colorString("green");
-	print STDERR "===============================\n";
-	print STDERR "Done!\n";
-	print STDERR colorString("reset");
+    print STDERR colorString("green");
+    print STDERR "===============================\n";
+    print STDERR "Done!\n";
+    print STDERR colorString("reset");
 } # end main()
 
 
@@ -77,8 +98,8 @@ main();
 
 
 END {
-  # Runs after everything else.
-  # Makes sure that the terminal text is back to its normal color.
+    # Runs after everything else.
+    # Makes sure that the terminal text is back to its normal color.
     print STDERR colorString("reset");
 }
 
