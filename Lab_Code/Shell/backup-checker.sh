ffa61d9288361ee187bf4c1767f1999efd5b4e6b
alexgw <alexgw@localhost>
2012-02-13 15:38:32 -0800

    Commit by alexgw

--- Lab_Code/Shell/backup-checker.sh
+++ Lab_Code/Shell/backup-checker.sh
@@ -1,7 +1,9 @@
 # -*-Sh-*- <-- tells emacs what kind of syntax highlighting to use
 
 
-STATUSFILE="${HOME}/backup-status.txt"
+STATUSFILE="${HOME}/backup-status-${HOSTNAME}.txt"
+CHECKSYNC=ionice -c3 rsync --dry-run --delete -vr
+
 echo ''
 echo "About to check the backups..."
 
@@ -9,12 +11,12 @@ echo "About to check the backups..."
 case "$HOSTNAME" in
     lighthouse)
 	echo "Checking the backups on the machine LIGHTHOUSE..."
-	ionice -c3 rsync --dry-run --delete -vr /db/ /it/lighthouse-backup/data/db/
+	${CHECKSYNC} /db/ /it/lighthouse-backup/data/db/ > ${STATUSFILE}
 	;;
 
     bueno)
 	echo "Checking the backups on the machine BUENO..."
-	ionice -c3 rsync --dry-run --delete -vr /home/ /it/bueno-backup/data/home/
+	${CHECKSYNC} /home/ /it/bueno-backup/data/home/ > ${STATUSFILE}
 	;;
     *)
 	echo 'We do not know how to check backups on any machine except bueno and lighthouse...'
