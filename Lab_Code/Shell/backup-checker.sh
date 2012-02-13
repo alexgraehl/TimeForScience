91708084093a105c8aaf5f820ef42eba23f89360
alexgraehl <alexgraehl@users.noreply.github.com>
2012-02-13 15:39:21 -0800

    Commit by alexgw

--- Lab_Code/Shell/backup-checker.sh
+++ Lab_Code/Shell/backup-checker.sh
@@ -2,7 +2,7 @@
 
 
 STATUSFILE="${HOME}/backup-status-${HOSTNAME}.txt"
-CHECKSYNC=ionice -c3 rsync --dry-run --delete -vr
+CHECKSYNC=sudo ionice -c3 rsync --dry-run --delete -vr
 
 echo ''
 echo "About to check the backups..."
@@ -11,12 +11,12 @@ echo "About to check the backups..."
 case "$HOSTNAME" in
     lighthouse)
 	echo "Checking the backups on the machine LIGHTHOUSE..."
-	${CHECKSYNC} /db/ /it/lighthouse-backup/data/db/ > ${STATUSFILE}
+	${CHECKSYNC} /db/ /it/lighthouse-backup/data/db/ >> ${STATUSFILE}
 	;;
 
     bueno)
 	echo "Checking the backups on the machine BUENO..."
-	${CHECKSYNC} /home/ /it/bueno-backup/data/home/ > ${STATUSFILE}
+	${CHECKSYNC} /home/ /it/bueno-backup/data/home/ >> ${STATUSFILE}
 	;;
     *)
 	echo 'We do not know how to check backups on any machine except bueno and lighthouse...'
