fee06f51fc322d412495a0634803f549d4d7a58f
alexgw <alexgw@localhost>
2012-02-13 15:46:47 -0800

    Commit by alexgw

--- Lab_Code/Shell/backup-checker.sh
+++ Lab_Code/Shell/backup-checker.sh
@@ -1,22 +1,27 @@
 # -*-Sh-*- <-- tells emacs what kind of syntax highlighting to use
-sudo ionice -c3 rsync --dry-run --delete -vr /home/ /it/bueno-backup/data/home/
+#sudo ionice -c3 rsync --dry-run --delete -vr /home/ /it/bueno-backup/data/home/
 
-echo "ARARA"
+#echo "ARARA"
 
 STATUSFILE="${HOME}/backup-status-${HOSTNAME}.txt"
-CHECKSYNC=sudo ionice -c3 rsync --dry-run --delete -vr
+CHECKSYNC="sudo ionice -c3 rsync --dry-run --delete -vr"
 
 echo ''
 echo "About to check the backups on the machine ${HOSTNAME}..."
+date
+echo ''
 
+echo '===================================' >> ${STATUSFILE}
+echo 'Starting new backup check:' >> ${STATUSFILE}
+date >> ${STATUSFILE}
+echo '' >> ${STATUSFILE}
 
 case "$HOSTNAME" in
     lighthouse)
 	${CHECKSYNC} /db/ /it/lighthouse-backup/data/db/ >> ${STATUSFILE}
 	;;
-
     bueno)
-	sudo ionice -c3 rsync --dry-run --delete -vr /home/ /it/bueno-backup/data/home/
+	${CHECKSYNC} /home/ /it/bueno-backup/data/home/ >> ${STATUSFILE}
 	;;
     *)
 	echo 'We do not know how to check backups on any machine except bueno and lighthouse...'
