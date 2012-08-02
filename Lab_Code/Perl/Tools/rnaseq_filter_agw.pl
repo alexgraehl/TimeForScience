779a04053391db34b1dbb755651ff3cafc099b58
alexgw <alexgw@localhost>
2012-08-01 17:04:59 -0700

    Commit by alexgw

--- Lab_Code/Perl/Tools/rnaseq_filter_agw.pl
+++ Lab_Code/Perl/Tools/rnaseq_filter_agw.pl
@@ -5,8 +5,10 @@
 
 ## This script handles a few basic processing steps for an aligned SAM or BAM file.
 
+## ==============================================================
 ## DETAILED README IS AT THE BOTTOM OF THIS FILE! SCROLL DOWN!
 ## (Or run this script with "--help" to see the full description.)
+## ==============================================================
 
 ## For example: sorting by coordinate order and generating a .BAI index file.
 ## The input to this script is any number of SAM or BAM files, and the output is, for each input file:
@@ -15,17 +17,10 @@
 ## If you give it an input file "input_name.sam", then the output filenames will be:
 ##   - "input_name.processed.bam" and "input_name.processed.bai"
 
-## TO DO:
-## - Should write the steps that are performed to the "summary" file. Currently the summary just has the number of reads in the before-processing file.
 
-
-use strict;  use warnings;
-use English '-no_match_vars';
-
-use Getopt::Long;
-use File::Basename;
-
-my $VERSION = "0.1.0"; ## Which version of the program is this. Change this if you update the program significantly.
+use strict;  use warnings;  use English '-no_match_vars';
+use Getopt::Long; use File::Basename;
+my $VERSION = "0.1.1"; ## Which version of the program is this. Change this if you update the program significantly.
 
 my $SUCCESS_STATUS = 0; ## <-- this should be ZERO
 
@@ -33,15 +28,14 @@ my $isDryRun = 0;
 my $shouldSort = 1;
 my $shouldFilterMappedOnly = 1;
 my $shouldRemoveDupes = 1;
-my $shouldCalculateSummary = 1;
-my $shouldGenerateIndex = 1;
+my $shouldQC = 1;
 #my $genomeFastaFile = undef;
 my $keepAllReads = 0;
 
-my $SAMTOOLS_PATH = `which samtools`;  chomp($SAMTOOLS_PATH);
-my $SORTSAM_PATH   = `which SortSam.jar`;  chomp($SORTSAM_PATH);
+my $SAMTOOLS_PATH = `which samtools`;  chomp($SAMTOOLS_PATH); ## Tries to find "samtools" on the user's $PATH
+my $SORTSAM_PATH   = `which SortSam.jar`;  chomp($SORTSAM_PATH); ## Tries to find "SortSam.jar" from the Picard suite in the user's $PATH
 my $GIGABYTES_FOR_PICARD = 2;
-my $MARKDUPLICATES_PATH = `which MarkDuplicates.jar`; chomp($MARKDUPLICATES_PATH);
+my $MARKDUPLICATES_PATH = `which MarkDuplicates.jar`; chomp($MARKDUPLICATES_PATH); ## Tries to find "MarkDuplicates.jar" from the Picard suite in the user's $PATH
 my $ULIMIT_RESULT = 1024; ## result of running the shell command ulimit -n. Since this is a shell built-in, it can, for some reason, not be run like a real command, so backticks don't work. `ulimit -n`; chomp($ULIMIT_RESULT);
 
 my @successfullyMadeFiles = (); # No files generated yet.
@@ -75,6 +69,18 @@ sub appendToSummaryFile($$) {
     } close(FILE);
 }
 
+sub getReadCountAndWriteSummary($$$) {
+    ## Given a BAM file and a message, it counts the number of reads in that file (using samtools), and then writes: message <\t> <count>.
+    ## Returns the count of reads.
+    my ($bamFile, $whatToWrite, $writeMessageToThisFile) = @_;
+    my $isBam = ($bamFile =~ m/[.]bam$/i);
+    if (!$isBam) { die "Problem: the input file <$bamFile> was not really a bam file (at least, it didn't end in '.bam'), so we can't read it! This is probably a PROGRAMMING error!\n\n[EXITING COMPLETELY]\n"; }
+    my $count = `samtools view -c $bamFile`; chomp($count); ## You can use "qx{}" instead of backticks if you want. This saves the result of the command.
+    ## $count should be an INTEGER, or else something is messed up!
+    appendToSummaryFile(($whatToWrite . "\t" . $count), $writeMessageToThisFile);
+    return $count; ## <-- return the count! Useful for further stuff.
+}
+
 sub reportCommandFailure($$) {
     my ($cmd, $fileThatFailed) = @_;
     datePrint("[ERROR]: In the processing for <$fileThatFailed>, this command failed: $cmd\n" . "We are skipping to the next file now.\n");
@@ -89,21 +95,29 @@ sub verifyToolPathsOrDie() {
     if (length($MARKDUPLICATES_PATH) <= 1) {  die "Could not find <Markduplicates.jar> (part of the Picard suite) in the \$PATH. Make sure the <MarkDuplicates.jar> java applet is somewhere in your \$PATH. You should be able to type \"MarkDuplicates.jar\" and get a weird java error message; if that is not the case, then this program won't run either. For installation instructions, check http://picard.sourceforge.net/.\nNote that MarkDuplicates.jar is PROBABLY not in your default path; try running `locate -i MarkDuplicates.jar` to find it. Then add it to somewhere that's in your path.\n"; }
 }
 
-
+sub generateIndex($$) {
+    my ($bamFile, $indexName) = @_;
+    my $indexCmd = qq{samtools index ${bamFile} ${indexName} }; ## Generates the file $indexName
+    my $result = alexSystemCall($indexCmd);
+    my $COMMAND_SUCCESSFUL_STATUS = 0;
+    if ($COMMAND_SUCCESSFUL_STATUS != $result) {
+	reportCommandFailure($indexCmd, $bamFile);
+    }
+    return ($result == $COMMAND_SUCCESSFUL_STATUS); ## Returns 1 if the command ran with no problems, 0 otherwise.
+}
 
 GetOptions("help|?|man"        => sub { printUsageAndQuit(); }
 	   , "sort!" => \$shouldSort ## specify "--nosort" to avoid sorting
 	   , "mapfilter!" => \$shouldFilterMappedOnly ## specify "--nomapfilter" to avoid filtering out the unmappable reads
 	   , "keepdupes" => sub { $shouldRemoveDupes = 0; }
 #	   , "f|fasta" => \$genomeFastaFile ## used for generating the index
-	   , "index!" => \$shouldGenerateIndex
-	   , "summary!" => \$shouldCalculateSummary
+	   , "qc!" => \$shouldQC
 	   , "keep|keepall!" => \$keepAllReads
 	   , "v|V|version|Version" => sub { printVersionAndQuit(); }
 	   , "dry|dryrun|dryRun|dry_run|dry-run" => \$isDryRun
     ) or printUsageAndQuit();
 
-if (scalar(@ARGV) < 1) { die "ARGUMENT ERROR: This script requires at least one filename---you need to give it a BAM or SAM file to operate on.\n"; }
+if (scalar(@ARGV) < 1) { die "ARGUMENT ERROR: This script requires at least one input filename---you need to give it a BAM or SAM file to operate on.\nRun it with --help in order to see the options.\n"; }
 
 print STDOUT "The following files will be processed by rnaseq_filter_agw:\n";
 foreach (@ARGV) {
@@ -141,11 +155,11 @@ foreach my $file (@ARGV) {
     }
 
     datePrint("Now processing the file <$file>...\n");
-    my $filteredFile     = "$file.filtered.tmp.bam";
-    my $sortedFile       = "$file.sorted_by_coord.tmp.bam";
-    my $dedupFile        = "$file.no_duplicates.tmp.bam";
-    my $dedupExtraMetricsFile = "$file.no_duplicates.extra.txt";
-    my $summaryStatsFile = "$file.summary.stats.txt";
+    my $filteredFile     = "$prefix.filtered.tmp.bam";
+    my $sortedFile       = "$prefix.sorted.tmp.bam";
+    my $dedupFile        = "$prefix.no_duplicates.tmp.bam";
+    my $dedupExtraMetricsFile = "$prefix.no_duplicates.extra.txt";
+    my $summaryStatsFile = "$prefix.summary.stats.txt";
     
     open FILE, ">", $summaryStatsFile or die $!; { ## <-- Note: NOT APPENDING -- clearing out the file completely!!!
 	print FILE "Summary file for <$file>:\n";
@@ -155,11 +169,9 @@ foreach my $file (@ARGV) {
 
     ### Now to actually RUN the various things ###
 
-
     if (!$isBam) {
-	appendToSummaryFile("As the very first step, converting SAM --> BAM for file <$latest>...\n", $summaryStatsFile);
-	my $bamName = $latest;
-	$bamName =~ s/[.]sam$/.bam/;
+	appendToSummaryFile("Converting SAM --> BAM for file <$latest>...\n", $summaryStatsFile);
+	my $bamName = $latest; $bamName =~ s/[.]sam$/.bam/;
 	my $convertToBamCmd = qq{samtools view -bS ${latest} > ${bamName} };
 	my $result = alexSystemCall($convertToBamCmd);
 	if ($result == $SUCCESS_STATUS) {
@@ -169,49 +181,48 @@ foreach my $file (@ARGV) {
 	    reportCommandFailure($convertToBamCmd, $latest);
 	    next; ## go to the next file...
 	}
-
     }
+
+    my $numReadsBeforeWeFiddledWithTheFile = getReadCountAndWriteSummary($latest, "Number of reads in <$latest> before any filtering:", $summaryStatsFile);
     
     ## Note: Picard sorting takes both SAM *and* BAM files, and outputs to BAM. So from here on out, we will be operating on BAM files only.
-    my $sortCmd = (qq{java -Xmx${GIGABYTES_FOR_PICARD}g -jar ${SORTSAM_PATH} }
-		   . qq{ INPUT=${latest} } ## Picard SortSam.jar accepts both SAM and BAM files as input!
-		   . qq{ SORT_ORDER=coordinate }
-		   . qq{ OUTPUT=${sortedFile} });
     if ($shouldSort) {
 	appendToSummaryFile("Sorting <$latest>...\n", $summaryStatsFile);
-	my $result = alexSystemCall($sortCmd);	
-	
+	my $sortCmd = (qq{java -Xmx${GIGABYTES_FOR_PICARD}g -jar ${SORTSAM_PATH} }
+		       . qq{ INPUT=${latest}  SORT_ORDER=coordinate  OUTPUT=${sortedFile} });
+	my $result = alexSystemCall($sortCmd);
 	if ($result == $SUCCESS_STATUS) {
-	    $latest = $sortedFile;
+	    $latest = $sortedFile; ## update the "latest" file to be this newly-sorted one
 	    appendToSummaryFile("Sorted <$latest> with Picard SortSam.jar.\n  Command was: $sortCmd\n", $summaryStatsFile);
 	} else {
 	    reportCommandFailure($sortCmd, $latest);
-	    datePrint("DEBUGGING MESSAGE FROM ALEX: Maybe the input file didn't have a SAM/BAM *header* line? The header is REQUIRED for sorting---check your input file ($latest) and make sure it has header lines. If it doesn't, then you'll need to re-header the file with samtools (`samtools reheader <in.header.sam> <in.bam>`).\n");
+	    datePrint("DEBUGGING MESSAGE FROM ALEX: Maybe the input file <$latest> didn't have a *header* line? The header is REQUIRED for sorting---check your input file ($latest) and make sure it has header lines. If it doesn't, then you'll need to re-header the file with samtools (`samtools reheader <in.header.sam> <in.bam>`). (That command assumes that you already have a properly-headered SAM file somewhere.)\n");
 	    next; ## go to the next file...
 	}
-	
     }
     
-    my $numReadsBeforeWeFiddledWithTheFile = "UNDEFINED";
-    my $countAllReadsCmd = qq{${SAMTOOLS_PATH} view -c ${latest} };
-    if ($shouldCalculateSummary) { 
-	# Count the number of reads in the file BEFORE we filter
-	# but AFTER we run the sorting command, to make sure the file is a BAM file.
-	$numReadsBeforeWeFiddledWithTheFile = `$countAllReadsCmd`;
-	appendToSummaryFile(("\nNumber of reads found in <$latest> before any filtering: " . $numReadsBeforeWeFiddledWithTheFile . "\n\n"), $summaryStatsFile);
-    }
-    
+
+    ## COUNTING TIME!
+    ## lower case -f means ONLY INCLUDE THESE READS. upper case -F means EXCLUDE THESE READS.
+    my $numFailedToMap       = `samtools view -c -f 0x004 $latest`;
+    my $numNonPrimary        = `samtools view -c -f 0x100 $latest`;
+    my $numFailedBasicQC     = `samtools view -c -f 0x200 $latest`;
+    my $numOpticalDuplicates = `samtools view -c -f 0x400 $latest`;
+    my $numMateDoesNotMap    = `samtools view -c -f 0x008 $latest`; ## <-- this one only occurs in paired-end reads
+
     ## -F means "skip flag"  (-f means "require flag")
     ## -F 0x0004 means "skip flag ID #4", which is "the query sequence itself is unmapped"
     ## -F 0x0008 means skip pairs where the MATE is unmapped . We are NOT skipping these for now.
     ## -F 0x0100 means "skip the 'not primary alignment' flag" (No secondary alignments)
     ## -F 0x0200 means "skip reads that fail vendor/platform quality control"
     ## -F 0x0400 means "skip optical / PCR duplicates"
-    my $samtoolsSkipFlags = qq{ -F 0x4  -F 0x100  -F 0x200  -F 0x400 };
-    my $filterMappedOnlyCmd = (qq{     samtools view -hbu -F 0x4  $latest } ## <-- only include the mapped (mapping flag is "4" apparently) reads, only include PRIMARY alignments, and skip any failing-QC reads
+    ## Note that the "-u" flag means "send the data to the next step in UNCOMPRESSED form."
+    ## The "-u" flag should be used for all these cases EXCEPT for the very last one, where we want the data compressed again.
+    my $filterMappedOnlyCmd = (qq{     samtools view -hbu -F 0x004  $latest } ## <-- only include the mapped (mapping flag is "4" apparently) reads, only include PRIMARY alignments, and skip any failing-QC reads
 			       . qq{ | samtools view -hbu -F 0x100 - } ## remove "alignment is not primary"
 			       . qq{ | samtools view -hbu -F 0x200 - } ## <-- remove "read fails QC"
-			       . qq{ | samtools view -hb -F 0x400 - } ## <-- remove "read is PCR or optical duplicate"
+			       . qq{ | samtools view -hbu -F 0x400 - } ## <-- remove "read is PCR or optical duplicate"
+			       . qq{ | samtools view -hb  -F 0x008 - } ## <-- remove "mate pair did not map" reads
 			       . qq{ > $filteredFile });   ## <-- output file location
     if ($shouldFilterMappedOnly) {
 	my $result = alexSystemCall($filterMappedOnlyCmd);
@@ -222,7 +233,7 @@ foreach my $file (@ARGV) {
 	    appendToSummaryFile(qq{  * Removed reads with flag 0x100 (\"alignment is not primary\")\n}, $summaryStatsFile);
 	    appendToSummaryFile(qq{  * Removed reads with flag 0x200 (\"read fails platform/vendor quality checks\")\n}, $summaryStatsFile);
 	    appendToSummaryFile(qq{  * Removed reads with flag 0x400 (\"read is either a PCR or an optical duplicate\")\n}, $summaryStatsFile);
-	    appendToSummaryFile(qq{  * We specifically KEPT reads where the *mate pair* (if present) is unmapped (0x8).\n}, $summaryStatsFile);
+	    appendToSummaryFile(qq{  * Removed reads with flag 0x8 (\"in paired-end data, the mate pair is unmapped\")\n}, $summaryStatsFile);
 	} else {
 	    reportCommandFailure($filterMappedOnlyCmd, $latest);
 	    next; ## go to the next file...
@@ -248,43 +259,24 @@ foreach my $file (@ARGV) {
 	}
     }
 
-    my $numReadsAfterProcessing = "UNDEFINED";
-    my $countAllReadsAfterProcessingCmd = qq{${SAMTOOLS_PATH} view -c ${latest} };
-    if ($shouldCalculateSummary) { 
-	# Count the number of reads in the file BEFORE we filter
-	# but AFTER we run the sorting command, to make sure the file is a BAM file.
-	$numReadsAfterProcessing = `$countAllReadsAfterProcessingCmd`;
-	appendToSummaryFile(("\nNumber of reads found in <$latest> after filtering: " . $numReadsAfterProcessing . "\n\n"), $summaryStatsFile);
-
-	my $fractRemaining = sprintf "%.3f", $numReadsAfterProcessing/$numReadsBeforeWeFiddledWithTheFile;
-	appendToSummaryFile(("\n(The fraction of remaining reads is: " . $fractRemaining . " )\n\n"), $summaryStatsFile);
-    }
+    my $numReadsAfterProcessing = getReadCountAndWriteSummary($latest, "Number of reads in <$latest> after filtering:", $summaryStatsFile);
+    my $fractRemaining = (sprintf "%.3f", $numReadsAfterProcessing/$numReadsBeforeWeFiddledWithTheFile);
+    appendToSummaryFile(("\n(The fraction of remaining reads is: " . $fractRemaining . " )\n\n"), $summaryStatsFile);
 
     my $finalBAM = "${prefix}.processed.bam";
     my $finalIndex = "${prefix}.processed.bai";
     
-    if ($shouldGenerateIndex) {
-	## This should always be the LAST step
-	my $indexCmd = qq{samtools index ${latest} ${finalIndex} };
-	my $result = alexSystemCall($indexCmd);
-	if ($result == $SUCCESS_STATUS) { 
-	    alexSystemCall(qq{ mv -f ${latest} $finalBAM });
-	} else {
-	    reportCommandFailure($indexCmd, $latest);
-	    next; ## go to the next file...
-	}
-    }
+    ## Index generation should always be the LAST processing step
+    my $indexWasGenerated = generateIndex($latest, $finalIndex);
+    if (!$indexWasGenerated) { next; } # skip to the next file if we failed this step.
+    
+    alexSystemCall(qq{ mv -f ${latest} $finalBAM }); ## Move the file to the done-processing-it location.
     
     push(@successfullyMadeFiles, $finalBAM);
-    if ($shouldGenerateIndex) {
-	datePrint("[Done] with <$file>. Generated the output files <${finalBAM}> and <${finalIndex}>\n\n");
-	push(@successfullyMadeFiles, $finalIndex);
-    } else {
-	datePrint("[Done] with <$file>. Generated the output file <${finalBAM}>\n\n");
-    }
+    datePrint("[Done] with <$file>. Generated the output files <${finalBAM}> and <${finalIndex}>\n\n");
+    push(@successfullyMadeFiles, $finalIndex);
     $numFilesSuccessfullyProcessed++;
 }
-
 
 if (scalar(@successfullyMadeFiles) > 0) {
     datePrint("*************************************************************\n");
@@ -309,10 +301,10 @@ __DATA__
 
 rnaseq_filter_agw.pl   [any number of bam/sam files may be specified here]
 
-Processes any number of input SAM or BAM alignment files, and by default:
-   - Removes any UNMAPPED reads
-   - SORTS the files in COORDINATE order
-   - Generates an output BAM file and a BAI index file.
+Processes input SAM/BAM alignment files, and:
+   1. Removes any UNMAPPED reads (unless you say --keepall)
+   2. SORTS the files in COORDINATE order (unless you say --nosort)
+   3. Generates an output BAM file and a BAI index file.
 
 The output is always a BAM file, regardless of the input.
 
@@ -331,9 +323,9 @@ OPTIONS:
    --dryrun:            Print what *would* be done, without doing it.
 
 FINE-TUNING OPTIONS:
-   --noindex:     Do not generate a bam index (Default: generates a ".bai" index file)
    --nosort:      Do not sort the input file (Default: output is sorted)
    --nomapfilter: Do not remove the unmapped reads (Default: unmappable reads are removed)
+   --noqc:        Do not run the QC metrics. Runs faster.
 
 Requires the following additional programs to already be installed AND in your $PATH:
     * These programs must be in your $PATH (even the Picard JARs must be on the path!)
