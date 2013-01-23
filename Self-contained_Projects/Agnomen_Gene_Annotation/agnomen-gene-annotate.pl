127fab3364c4c71c9f42acc2cf066231fd908836
alexgw <alexgw@localhost>
2013-01-22 21:20:16 -0800

    Commit by alexgw

--- Self-contained_Projects/Agnomen_Gene_Annotation/agnomen-gene-annotate.pl
+++ Self-contained_Projects/Agnomen_Gene_Annotation/agnomen-gene-annotate.pl
@@ -16,11 +16,11 @@ use strict;  use warnings;  use diagnostics;
 sub main();
 
 my $INTERSECT_BED_EXE = "/work/Apps/Bio/bedtools/bin/intersectBed";
-my $LOCAL_ANNOTATION_DIR = "./Z_AGNOMEN_DATA";
+my $globalAnnotDir = "./Z_AGNOMEN_DATA"; # default!
 
 my $AGNOMEN_ENSEMBL_GRABBER_SCRIPT = "/work/Common/Code/ProjectCode/0_Analyze_0277_Agnomen_Annotate/agnomen-ensembl-hg19-grabber.pl";
 
-my %GLOBAL_INTERSECT_FILES = (); ## New hash that will keep track of all the annotation files. Keys are the descriptions of the files, values are the actual paths of the annotation files.
+my %GLOBAL_ANNOT_FILES = (); ## New hash that will keep track of all the annotation files. Keys are the descriptions of the files, values are the actual paths of the annotation files.
 
 sub systemBash {
     my @args = ( "bash", "-c", shift );
@@ -49,18 +49,110 @@ sub quitWithUsageError($) { print($_[0] . "\n"); printUsage(); print($_[0] . "\n
 sub printUsageAndQuit() { printUsage(); exit(1); }
 sub printUsage() {  print STDOUT <DATA>; }
 
+
+
+sub agnomenRemoteDownloadAnnot($$$$;$$) {
+    # Tries to generate the file $finalName, UNLESS that file already exists and is valid.
+    ## Arguments:
+    ## 0. The "short name" that gets used as the hash ID and also in the filename of the output
+    ## 1. The URL to get
+    ## 2. the genome build (e.g. hg19, hg18, mm9)
+    ## 3. the name to save the RAW file into
+    ## 4. postprocessingCmd: something to do with the file after downloading it. Optional
+    ## 5. finalName: the final filename after processing
+    my ($shortName, $url, $theBuild, $rawNameIncludingCompression, $postprocessingCmd, $finalName) = @_;
+
+    my $localDir = "${globalAnnotDir}/${theBuild}"; #${species}/${theBuild}";
+    my $rawFullPath = $localDir . "/" . $rawNameIncludingCompression;
+    my $unzippedPath = $rawFullPath;
+
+    if ($rawFullPath =~ m/[ ,\t\s]/) { die "Uh oh, the local full path has a comma or whitespace in it, which is not legal for the directory name!!"; }
+    my $curlCmd = 'curl --remote-name ' . '"' . $url . '"' ;
+
+    system("mkdir -p $localDir");
+    if ($rawNameIncludingCompression ne File::Basename::basename($url)) {
+	stderrPrint("This is unusual; the local name of the file ($rawNameIncludingCompression) does not match the filename from the URL (the end of this URL: $url)!");
+    }
+
+    $unzippedPath =~ s/[.](bz2|gz|zip)$//i;
+    if ((-e $rawFullPath && (-s $rawFullPath > 0)) || (-e $unzippedPath && (-s $unzippedPath > 0))) {
+	stderrPrint(colorString("cyan"));
+	stderrPrint("[Skipping] re-download of <$rawFullPath> from <$url> -- file (or an uncompressed version of that file) already exists\n");
+	stderrPrint(colorString("reset"));
+    } else {
+	stderrPrint("Downloading $url --> $rawFullPath...\n");
+	my $status = system($curlCmd);
+	if ($status != 0) {
+	    die "Uh oh, download status was not zero! Something went wrong in the download of $url -> $rawFullPath.\nEXITING PROGRAM NOW.\n";
+	}
+	if ($rawFullPath =~ m/[.]gz$/) { system("gunzip --stdout $rawFullPath > $unzippedPath"); }
+	if ($rawFullPath =~ m/[.]bz2$/) { system("bunzip2 --stdout $rawFullPath > $unzippedPath"); }
+	if ($rawFullPath =~ m/[.]zip$/) { system("unzip $rawFullPath"); }
+	stderrPrint("[Done] Successfully downloaded a file to the probably-uncompressed location <$unzippedPath>\n");
+    }
+
+    ## Now the file should be downloaded, if necessary. We may also have to do a post-processing step, below.
+    
+    if (defined($postprocessingCmd) && $postprocessingCmd && length($postprocessingCmd) > 0) {
+	if ((not -e $finalName || (-s $finalName == 0))) {
+	    ## processed file already exists
+	    stderrPrint(colorString("cyan"));
+	    stderrPrint("[Skipping] re-generation of $finalName, as the output file already exists.\n");
+	    stderrPrint(colorString("reset"));
+	} else {
+	    stderrPrint("Now running this post-processing command: $postprocessingCmd\n");
+	    my $status = system($postprocessingCmd);
+	    if ($status != 0) {
+		die "Uh oh, system code status from the postprocessing command was not zero! Something went wrong in the running of the code <$postprocessingCmd>. Quitting...\n";
+	    }
+	    if (not -e $finalName) { die "Uh oh, we failed to generate the file <$finalName>! Maybe the shell command had a mis-typed final filename, or perhaps something else went wrong?\n"; }
+	}
+	$GLOBAL_ANNOT_FILES{$shortName} = $finalName; ## <-- Add this file to the list of annotation files.
+    } else {
+	# No postprocessing to do, so we just want to use the regular file as an annotation file
+	$GLOBAL_ANNOT_FILES{$shortName} = $unzippedPath;
+    }
+}
+
+sub agnomenGenerateLocalAnnot($$$$$) {
+    # Doesn't try to download anything; just runs '$cmd' UNLESS the $finalFilename already is a non-zero-length file.
+    my ($shortName, $finalFilename, $theBuild, $cmd) = @_;
+    my $localDir = "${globalAnnotDir}/${theBuild}";	
+    my $finalFullPath = $localDir . "/" . $finalFilename;
+    if ((-e $finalFullPath && (-s $finalFullPath > 0))) {
+	stderrPrint(colorString("cyan"));
+	stderrPrint("[Skipping] re-generation of file <$finalFilename>, which already exists in <$finalFullPath>!\n");
+	stderrPrint(colorString("reset"));
+    } else {
+	my $fullCommand = "cd $localDir && $cmd";
+	stderrPrint("[Generating the file $finalFilename], by running the command:\n$fullCommand\n\n\nRunning now...\n");
+	my $status = system($fullCommand); ## cd into the local
+	if ($status != 0) {
+	    die "Uh oh, system code status from the postprocessing command was not zero! Something went wrong in the running of the code <$fullCommand>. Quitting...\n";
+	}
+    }
+    if (exists($GLOBAL_ANNOT_FILES{$shortName})) { die "Programming error 19A: Uh oh, there is ALREADY an annotation file with the short name <$shortName>. Can't have two duplicate short names! Check the source code and fix it.\n"; }
+    $GLOBAL_ANNOT_FILES{$shortName} = $finalFullPath;
+}
+
+
+
+
+
+
 # ==1==
 sub main() { # Main program
     my ($delim) = "\t";
-    my ($species) = undef;
+    my ($genomeBuild) = undef;
     my ($shouldUpdate) = 0;
     my ($decimalPlaces) = 4; # How many decimal places to print, by default
     $Getopt::Long::passthrough = 1; # ignore arguments we don't recognize in GetOptions, and put them in @ARGV
 
     GetOptions("help|?|man" => sub { printUsageAndQuit(); }
 	       , "delim|d=s" => \$delim
-	       , "species|s=s" => \$species
+	       , "species|s=s" => \$genomeBuild
 	       , "update!" => \$shouldUpdate
+	       , "annotdir|a" => \$globalAnnotDir
 	       , "dp=i" => \$decimalPlaces
 	) or printUsageAndQuit();
 
@@ -82,7 +174,7 @@ sub main() { # Main program
 #	quitWithUsageError("Error in arguments! You must send TWO filenames to this program.\n");
 #    }
 
-    my @inputFiles = ();
+    my @inputFiles = (); # Input files to annotate
 
     foreach (@ARGV) { # these were arguments that were not understood by GetOptions
 	if (-e $_) {
@@ -92,105 +184,30 @@ sub main() { # Main program
 	}
     }
 
+    unless (-d $globalAnnotDir) { quitWithUsageError(">>> ERROR >>> The annotation directory ($globalAnnotDir) does not exist!") }
+    unless (defined($genomeBuild) && (length($genomeBuild) > 0)) { quitWithUsageError(">>> ERROR >>> it is MANDATORY to specify a genome build (example: hg19 or mm9). You do this with a command like this:   --species=hg19  or  -s hg19 \nCheck the annotation directory for a list of valid species.\n"); }
+
+    stderrPrint("* Searching for annotation files in: <$globalAnnotDir>\n");
+    stderrPrint("* Using the specified genome build: <$genomeBuild>\n");
+    stderrPrint("* Searching for BED annotation files in the following file path: <$globalAnnotDir/$genomeBuild/>\n");
+
     if (scalar(@inputFiles) == 0 && !$shouldUpdate) {
 	## Need to have some files to annotate!
 	quitWithUsageError(">>> ERROR >>> We expected AT LEAST ONE valid file to annotate to be specified on the command line. We didn't get any, however.\n");
     }
 
-    sub agnomenRemoteDownloadAnnot($$$$;$$) {
-	## Arguments:
-	## 0. The "short name" that gets used as the hash ID and also in the filename of the output
-	## 1. The URL to get
-	## 2. the species
-	## 3. the build (e.g. hg19, hg18, mm9)
-	## 4. the name to save the RAW file into
-	## 5. postprocessingCmd: something to do with the file after downloading it. Optional
-	## 6. processedFilename: the final filename after processing
-	my ($shortName, $url, $species, $build, $rawNameIncludingCompression, $postprocessingCmd, $processedFilename) = @_;
-	my $curlCmd = 'curl --remote-name ' . '"' . $url . '"' ;
-	my $localDir = "${LOCAL_ANNOTATION_DIR}/${species}/${build}";
-	system("mkdir -p $localDir");
-	if ($rawNameIncludingCompression ne File::Basename::basename($url)) {
-	    stderrPrint("This is unusual; the local name of the file does not match the URL name!");
-	}
-	my $rawFullPath = $localDir . "/" . $rawNameIncludingCompression;
-	if ($rawFullPath =~ m/[ ,\t\s]/) { die "Uh oh, the local full path has a comma or whitespace in it, which is not legal for the directory name!!"; }
-	my $unzippedPath = $rawFullPath;
-	$unzippedPath =~ s/[.](bz2|gz|zip)$//i;
-	if ((-e $rawFullPath && (-s $rawFullPath > 0)) || (-e $unzippedPath && (-s $unzippedPath > 0))) {
-	    stderrPrint(colorString("cyan"));
-	    stderrPrint("[Skipping] re-download of <$rawFullPath> from <$url> -- file (or an uncompressed version of that file) already exists\n");
-	    stderrPrint(colorString("reset"));
-	} else {
-	    stderrPrint("Downloading $url --> $rawFullPath...\n");
-	    my $status = system($curlCmd);
-	    if ($status != 0) {
-		die "Uh oh, download status was not zero! Something went wrong in the download of $url -> $rawFullPath.\nEXITING PROGRAM NOW.\n";
-	    }
-	    if ($rawFullPath =~ m/[.]gz$/) { system("gunzip --stdout $rawFullPath > $unzippedPath"); }
-	    if ($rawFullPath =~ m/[.]bz2$/) { system("bunzip2 --stdout $rawFullPath > $unzippedPath"); }
-	    if ($rawFullPath =~ m/[.]zip$/) { system("unzip $rawFullPath"); }
-	    stderrPrint("[Done] Successfully downloaded a file to the probably-uncompressed location <$unzippedPath>\n");
-	}
-
-	## Now the file should be downloaded, if necessary. We may also have to do a post-processing step, below.
-	
-	if (defined($postprocessingCmd) && $postprocessingCmd && length($postprocessingCmd) > 0) {
-	    if ((not -e $processedFilename || (-s $processedFilename == 0))) {
-		## processed file already exists
-		stderrPrint(colorString("cyan"));
-		stderrPrint("[Skipping] re-generation of $processedFilename, as the output file already exists.\n");
-		stderrPrint(colorString("reset"));
-	    } else {
-		stderrPrint("Now running this post-processing command: $postprocessingCmd\n");
-		my $status = system($postprocessingCmd);
-		if ($status != 0) {
-		    die "Uh oh, system code status from the postprocessing command was not zero! Something went wrong in the running of the code <$postprocessingCmd>. Quitting...\n";
-		}
-		if (not -e $processedFilename) { die "Uh oh, we failed to generate the file <$processedFilename>! Maybe the shell command had a mis-typed final filename, or perhaps something else went wrong?\n"; }
-	    }
-	    $GLOBAL_INTERSECT_FILES{$shortName} = $processedFilename; ## <-- Add this file to the list of annotation files.
-	} else {
-	    # No postprocessing to do, so we just want to use the regular file as an annotation file
-	    $GLOBAL_INTERSECT_FILES{$shortName} = $unzippedPath;
-	}
-    }
-    
-    sub agnomenGenerateLocalAnnot($$$$$;$$) {
-	# Doesn't try to download anything; just runs '$cmd'
-	my ($shortName, $finalFilename, $species, $build, $cmd) = @_;
-	my $localDir = "${LOCAL_ANNOTATION_DIR}/${species}/${build}";	
-	my $finalFullPath = $localDir . "/" . $finalFilename;
-	if ((-e $finalFullPath && (-s $finalFullPath > 0))) {
-	    stderrPrint(colorString("cyan"));
-	    stderrPrint("[Skipping] re-generation of file <$finalFilename>, which already exists in <$finalFullPath>!\n");
-	    stderrPrint(colorString("reset"));
-	} else {
-	    my $fullCommand = "cd $localDir && $cmd";
-	    stderrPrint("[Generating the file $finalFilename], by running the command:\n$fullCommand\n\n\nRunning now...\n");
-	    my $status = system($fullCommand); ## cd into the local
-	    if ($status != 0) {
-		die "Uh oh, system code status from the postprocessing command was not zero! Something went wrong in the running of the code <$fullCommand>. Quitting...\n";
-	    }
-	}
- 	if (exists($GLOBAL_INTERSECT_FILES{$shortName})) { die "Programming error 19A: Uh oh, there is ALREADY an annotation file with the short name <$shortName>. Can't have two duplicate short names! Check the source code and fix it.\n"; }
-	$GLOBAL_INTERSECT_FILES{$shortName} = $finalFullPath;
-    }
     
     if ($shouldUpdate) {
-    my $DOLLAR_SIGN = '$';
-    agnomenRemoteDownloadAnnot("Mouse_Ensembl_GTF", "ftp://ftp.ensembl.org/pub/release-68/gtf/mus_musculus/Mus_musculus.GRCm38.68.gtf.gz", "mouse", "mm9", "Mus_musculus.GRCm38.68.gtf.gz");
-    
-    agnomenGenerateLocalAnnot("Human_Ensembl_BED"
-			      , "human_ensembl_via_perl_api.tab", "human", "hg19", 
-			      ( qq{ if [ ! -f human_ensembl_1_raw.tab.gz ]; then echo "\n>>>[NOTE] -- The ENSEMBL GRABBER takes about 18 hours to download data via Perl API! If you are reading this message, be prepared to wait QUITE A WHILE for the data to get downloaded!\n"; $AGNOMEN_ENSEMBL_GRABBER_SCRIPT | gzip > human_ensembl_1_raw.tab.gz ; fi; }
-				. qq{ zcat human_ensembl_1_raw.tab.gz | grep -v '^[#]' | grep -v '^$DOLLAR_SIGN' | sort -k 2,2 -k 3,3g | uniq > human_ensembl_2.tmp ; }
-				. qq{ sed 's/^/chr/' human_ensembl_2.tmp > human_ensembl_3.tmp ; } ## add 'chr' to the beginning of each line
-				. qq{ cat human_ensembl_2.tmp >> human_ensembl_3.tmp ; }
-				. qq{ sort human_ensembl_3.tmp > human_ensembl_via_perl_api.tab ; }
-				. qq{ /bin/rm human_ensembl_[123].tmp ; }
-			      )
-	);
+	my $DOLLAR_SIGN = '$';
+	agnomenRemoteDownloadAnnot("Mouse_Ensembl_GTF", "ftp://ftp.ensembl.org/pub/release-68/gtf/mus_musculus/Mus_musculus.GRCm38.68.gtf.gz", "mm9", "Mus_musculus.GRCm38.68.gtf.gz", undef, "Mus_musculus.GRCm38.68.gtf");
+	agnomenGenerateLocalAnnot("Human_Ensembl_BED", "human_ensembl_via_perl_api.tab", "hg19",
+				  ( qq{ if [ ! -f human_ensembl_1_raw.tab.gz ]; then echo "\n>>>[NOTE] -- The ENSEMBL GRABBER takes about 18 hours to download data via Perl API! If you are reading this message, be prepared to wait QUITE A WHILE for the data to get downloaded!\n"; $AGNOMEN_ENSEMBL_GRABBER_SCRIPT | gzip > human_ensembl_1_raw.tab.gz ; fi; }
+				    . qq{ zcat human_ensembl_1_raw.tab.gz | grep -v '^[#]' | grep -v '^$DOLLAR_SIGN' | sort -k 2,2 -k 3,3g | uniq > human_ensembl_2.tmp ; }
+				    . qq{ sed 's/^/chr/' human_ensembl_2.tmp > human_ensembl_3.tmp ; } ## add 'chr' to the beginning of each line
+				    . qq{ cat human_ensembl_2.tmp >> human_ensembl_3.tmp ; }
+				    . qq{ sort human_ensembl_3.tmp > human_ensembl_via_perl_api.tab ; }
+				    . qq{ /bin/rm human_ensembl_[123].tmp ; }
+				  ));
     }
     
     #print "\t" . sprintf("%.${numDecimalPointsToPrint}f", $levFraction);
@@ -199,7 +216,7 @@ sub main() { # Main program
     #my $annot1 = "AnnotatedFeatures.gff"; #"hb.bed"; #"human_ens_manual.bed"; #"b.gtf"; #"AnnotatedFeatures.gff"; #"annot.bed";
     #stderrPrint("Warning: using a manually-selected annotation file here.\n");
 
-    if (scalar(keys(%GLOBAL_INTERSECT_FILES)) == 0) {
+    if (scalar(keys(%GLOBAL_ANNOT_FILES)) == 0) {
 	die "Uh oh, there were no annotation files, for some reason! Maybe you need to run 'agnomen-gene-annotate.pl --update' to re-download / re-generate them?\n";
     }
     
@@ -208,12 +225,11 @@ sub main() { # Main program
     for my $input (@inputFiles) {
 	if ($input !~ m/[.]bed$/i) {
 	    ## Expecting the $input to be a BED file--- check to see if it has ".bed" as an extension?
-	    stderrPrint("Warning: the input file $input was EXPECTED to be a .bed file, but it appears not to have the .bed filename extension. Double-check to make sure this is the right file! Continuing on anyway...\n");
+	    stderrPrint("Warning: the input file $input was EXPECTED to be a .bed file, but it appears not to have the .bed filename extension. Double-check to make sure this is the right file! Continuing on anyway, assuming that it is a bed file...\n");
 	}
 	## 
-	while (my($annotDescription, $annotFile) = each(%GLOBAL_INTERSECT_FILES)) {
+	while (my($annotDescription, $annotFile) = each(%GLOBAL_ANNOT_FILES)) {
 	    ## annotFile better exist, or we're in trouble!!
-
 #	    if ($annotFile =~ m/.gz$/) {
 #		$annotFile = "<(zcat $annotFile)"; # bash subshell
 #		print STDERR "SUBSHELL\n";
@@ -256,22 +272,28 @@ exit(0);
 
 __DATA__
 
-agnomen-gene-annotate.pl  [OPTIONS]  [-s SPECIES]   INPUT_FILE_TO_ANNOTATE.bed
+agnomen-gene-annotate.pl  [OPTIONS]  -s GENOME_BUILD   INPUT_FILE_TO_ANNOTATE.bed
 
 by Alex Williams, 2012
 
+GENOME_BUILD must be a UCSC-style genome build (examples: hg19, mm9)
+
+The input file to annotation must be a valid tab-delimited BED-format file.
+
+Example BED file:
+    chr1   <tab>   100    <tab>   200
+    chr2   <tab>  1150    <tab>  1300
+
 If you want to learn more about file formats (GFF, etc), check: http://genome.ucsc.edu/FAQ/FAQformat.html
 
-See the examples below for more information.
-
-The local annotation is expected to be saved in a directory called Z_AGNOMEN_DATA.
+The local annotation is, by default, expected to be found in a directory called Z_AGNOMEN_DATA.
 
 Agnomen is supposed to be able to manage the Z_AGNOMEN_DATA directory by itself, so you should in theory
 not have to deal with this directory.
 
 EXAMPLES:
 
-agnomen-gene-annotate.pl  somefile.bed
+agnomen-gene-annotate.pl  -s hg19  somefile.bed
   Annotates "somefile.bed" and outputs a new annotated file for each annotation type:
   Example output files that might be generated:
               ANNOT_somefile.bed--HUMAN_TRANSCRIPTS.txt
@@ -292,8 +314,17 @@ OPTIONS:
 
   --species = STRING  (Default: undefined)
     or: -s STRING
-       Sets which species should be used for annotation.
+       Sets which genome build should be used for annotation. Example: hg19 (a human genome), mm9 (a mouse genome).
+       Currently supported: hg19, mm9.
        Default is to use output for ALL the different species.
+
+  --annotdir = STRING (Default: Z_AGNOMEN_DATA)
+    or: -a STRING
+       A path on the filesystem where we keep the annotation data. The annotation data must be in species-specific
+       sub-folders. Example filesystem:  MY_DATA/hg19/human_annot.bed
+                                         MY_DATA/mm10/mouse_annot_1.bed
+                                         MY_DATA/mm10/mouse_annot_extended.bed
+       In the example above, you would specify --annotdir=MY_DATA
 
   --update
        Tells Agnomen to check for (and potentially update) newer annotation files. Does not fully work yet!
