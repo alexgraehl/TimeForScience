759c04cb5f1dbda9f8ce2e9f222c5177d15b6558
alexgraehl <alexgraehl@users.noreply.github.com>
2013-03-15 14:48:41 -0700

    Commit by alexgw

--- Self-contained_Projects/Agnomen_Gene_Annotation/gtf-to-extra-annotated-bed.R
+++ Self-contained_Projects/Agnomen_Gene_Annotation/gtf-to-extra-annotated-bed.R
@@ -1,5 +1,7 @@
 # options(error=recover)
 
+library(IRanges) # from Bioconductor. REQUIRED!
+
 trimWhitespaceBothEnds <- function(string) { gsub("^\\s+|\\s+$", '', string) }
 
 print("Ok...")
@@ -27,6 +29,27 @@ column9 <- strsplit(tomato[, FREETEXT_COL_IDX], FREETEXT_DELIM, perl=T)
 # exon_id
 # exon_number
 # gene_name
+
+xprint <- function(...) {
+     print(paste(..., sep=''))
+}
+
+featureIsEntirelyBetween <- function(irange1, irange2) {
+     return(length(IRanges::findOverlaps(irange1, irange2, type="within")) > 0) # Type: WITHIN only
+}
+
+featureIsPartiallyWithin <- function(irange1, irange2) {
+     return(length(IRanges::findOverlaps(irange1, irange2)) > 0)
+}
+
+featureIsEntirelyOutside <- function(irange1, irange2) {
+     return(0 == length(IRanges::findOverlaps(irange1, irange2)))
+}
+
+rangeStr <- function(irange) {
+     # returns the string for a range. Example: [1094-2094] (101) . Note that the width is INCLUSIVE! (e.g. 200 to 300 has 101 elements.)
+     return(paste("[", irange@start, "-", (irange@start+irange@width-1), " width=", irange@width, "]", sep=''))
+}
 
 devour <- function(key, searchInThisVector) {
      # Looks for the KEY (a perl search string!) in the split-up free text vector
@@ -66,8 +89,20 @@ for (ut in uniqTS.vec) {
      b.frame <- a.frame[ order(a.frame$START), ] # re-order the data frame by start positions of each exon/element!
      nStartCodons = sum(b.frame$TYPE == "start_codon")
      nStopCodons  = sum(b.frame$TYPE == "stop_codon")
+
+     strand = unique(b.frame$STRAND)
+     isPositiveStrand = (strand == "+" || strand == "1" || strand == "+1")
+     stopifnot(length(strand) == 1)
+
+     print(paste("Strand: ", strand, sep=''))
+     
      print( paste("Transcript <", ut, "> had ", nStartCodons, " start codons (", rep("S", times=nStartCodons),  ") and ", nStopCodons, " stop codons (", rep("E", times=nStopCodons), ").", sep='') )
 
+     if (nStartCodons != nStopCodons) {
+          print("WARNING: UNEQUAL NUMBER OF START & STOP CODONS. This appears to be a common situation, so I guess it is by design?")
+          #stopifnot(nStartCodons == nStopCodons)
+     }
+     
      # Any exons BEFORE the start codon are 5' UTR
      
      # Any exons AFTER the stop codon are 3' UTR
@@ -76,14 +111,44 @@ for (ut in uniqTS.vec) {
      #              and are 2) between the first and last exons
      #                         are introns.
      
-     lowerLimit <- min( b.frame$START, b.frame$END ) # lowest-numbered base pair. Disregards strand.
-     upperLimit <- max( b.frame$START, b.frame$END ) # highest-numbered base pair. Disregards strand.
+     #lowerLimit <- min( b.frame$START, b.frame$END ) # lowest-numbered base pair of ANY type. Disregards strand.
+     #upperLimit <- max( b.frame$START, b.frame$END ) # highest-numbered base pair of ANY type. Disregards strand.
+
+     BIGGEST_POSSIBLE_GENOMIC_COORDINATE <- (.Machine$integer.max - 100) # This needs to be greater than any valid genomic coordinate, but not so big as to instantly cause integer overflow. What a mess. Why did I use IRanges anyway!!!
      
-     
+     codingLimitLeft  <- -1 # any invalid genomic coordinate
+     codingLimitRight <- (codingLimitLeft-1) # one less than codingLimitLeft, so that IRanges is happy. CANNOT be more than one unit less!
+     if (isPositiveStrand && nStartCodons > 0) {
+          codingLimitLeft  = as.numeric(b.frame[b.frame$TYPE == "start_codon", "START"])
+          codingLimitRight = ifelse(nStopCodons == 0, yes=BIGGEST_POSSIBLE_GENOMIC_COORDINATE, no=as.numeric(b.frame[b.frame$TYPE == "stop_codon", "STOP"]))
+     } else if (!isPositiveStrand && nStartCodons > 0) {
+          codingLimitRight = as.numeric(b.frame[b.frame$TYPE == "start_codon", "STOP"])
+          codingLimitLeft  = ifelse(nStopCodons == 0, yes=-1, no=as.numeric(b.frame[b.frame$TYPE == "start_codon", "START"]))
+     }
+
+     codingRange <- IRanges(start=codingLimitLeft, end=codingLimitRight)
+     print( paste("Anything within ", rangeStr(codingRange), " is within the coding sequence.", sep=''))
      
      for (rrr in seq_along(rownames(b.frame))) {
-          thisItem <- b.frame[rrr, ]
-          #if (thisItem$START
+          rowItem <- b.frame[rrr, ]
+          queryRange <- IRanges(start=rowItem$START, end=rowItem$STOP)  # IRanges::intersect(IRanges(10,20), IRanges(40,51))
+
+          #print( paste("Query: ", codingRange@start, " and ", (codingRange@start + codingRange@width - 1), " is within the coding sequence.", sep=''))
+          if (featureIsEntirelyBetween(queryRange, codingRange)) {
+               print(paste("Feature at ", rangeStr(queryRange), " is entirely within the coding limits!", sep=''))
+          } else if (featureIsEntirelyOutside(queryRange, codingRange)) {
+               print(paste("OUTSIDE at ", rangeStr(queryRange), " -------------------", sep=''))
+          } else {
+               print(paste("Partially inside at ", rangeStr(queryRange), " ****", sep=''))
+          }
+
+          qCode    <- IRanges::intersect(queryRange, codingRange) # only the part of the query that is ALSO in the coding region
+          uCode    <- IRanges::setdiff(queryRange, codingRange) # subtract out the coding part from the query
+
+          xprint("Coding part of that: ", rangeStr(qCode))
+          xprint("UTR part of that: ", rangeStr(uCode))
+          
+          #if (queryRange$START
      }
      
 }
