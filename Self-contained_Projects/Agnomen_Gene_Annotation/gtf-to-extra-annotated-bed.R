3d4d77d5df40fde5517677c190b66a0437a79fec
alexgw <alexgw@localhost>
2013-03-22 16:13:54 -0700

    Commit by alexgw

--- Self-contained_Projects/Agnomen_Gene_Annotation/gtf-to-extra-annotated-bed.R
+++ Self-contained_Projects/Agnomen_Gene_Annotation/gtf-to-extra-annotated-bed.R
@@ -8,14 +8,20 @@ print("Ok...")
 
 if (!exists("hugeData")) {
      print("Loading a huge GTF file!")
-     hugeData <- read.table("Homo_sapiens.GRCh37.70.gtf.txt", sep="\t", stringsAsFactors=FALSE)
+     hugeData <- read.table("Homo_sapiens.GRCh37.70.gtf.txt", sep="\t"
+                            , stringsAsFactors=F
+#                            , stringsAsFactors=TRUE
+#                            , colClasses=c("factor","factor","factor" # CHROMOSOME, type (e.g. "retained_intron"), exon/CDS/whatever
+#                              , "integer", "integer", "character" # START, STOP, SCORE (usually score is just a '.')
+#                              , "factor", "factor", "character") # STRAND, something, character
+                            )
      print("Loaded the huge GTF file!")
 }
 
 #TESTRANGE <- 12250:12255
-#TESTRANGE <- 12250:12355
-#tomato <- hugeData[TESTRANGE,]
-tomato <- hugeData
+TESTRANGE <- 1000:1500
+tomato <- hugeData[TESTRANGE,]
+#tomato <- hugeData
 
 FREETEXT_COL_IDX <- 9
 
@@ -42,10 +48,11 @@ xPrintRow <- function(type, range, rowItem) {
 xPrintRowRaw <- function(type, range, rowItem) {
      # Print ths row, but without the type, start, stop, and exon/exnum fields.
      # Used for printing introns and UTRs, which don't have valid data for those files in the input row data.
-     
+     #                      1   2        3     4       5      6         7     8     9     10      11
      #colnames(ddd) <- c("CHR","ABOUT","TYPE","START","STOP","STRAND","GENE","TS","EXON","EXNUM","COMMON")
      cat(paste(type, rangeStrTab(range)
-               , paste(c(rowItem$CHR, rowItem$ABOUT, "NA", "NA", "NA", rowItem$STRAND, rowItem$GENE, rowItem$TS, "NA", "NA", rowItem$COMMON), collapse="\t")
+               , paste(c(rowItem[1], rowItem[2], "NA", "NA", "NA", rowItem[6], rowItem[7], rowItem[8], "NA", "NA", rowItem[11]), collapse="\t")
+               #, paste(c(rowItem["CHR"], rowItem["ABOUT"], "NA", "NA", "NA", rowItem["STRAND"], rowItem["GENE"], rowItem["TS"], "NA", "NA", rowItem["COMMON"]), collapse="\t")
                , "\n", sep="\t"))
 }
 
@@ -55,7 +62,8 @@ xPrintRowExceptStartStop <- function(type, range, rowItem) {
      # Used for printing the split-up exons that are partially coding and partially UTR.
      #colnames(ddd) <- c("CHR","ABOUT","TYPE","START","STOP","STRAND","GENE","TS","EXON","EXNUM","COMMON")
      cat(paste(type, rangeStrTab(range)
-               , paste(c(rowItem$CHR, rowItem$ABOUT, rowItem$TYPE, "NA", "NA", rowItem$STRAND, rowItem$GENE, rowItem$TS, rowItem$EXON, rowItem$EXNUM, rowItem$COMMON), collapse="\t")
+               , paste(c(rowItem[1], rowItem[2], rowItem[3], "NA", "NA", rowItem[6], rowItem[7], rowItem[8], rowItem[9], rowItem[10], rowItem[11]), collapse="\t")
+               #, paste(c(rowItem["CHR"], rowItem["ABOUT"], rowItem["TYPE"], "NA", "NA", rowItem["STRAND"], rowItem["GENE"], rowItem["TS"], rowItem["EXON"], rowItem["EXNUM"], rowItem["COMMON"]), collapse="\t")
                , "\n", sep="\t"))
 }
 
@@ -113,13 +121,38 @@ devour <- function(key, searchInThisVector) {
 }
 
 
+## handleARow <- function(rowItem) {
+##      # "type" index is 3
+##      # start index is 4, stop index is 5
+##      queryRange <- IRanges(start=as.numeric(rowItem[4]), end=as.numeric(rowItem[5]))  # IRanges::intersect(IRanges(10,20), IRanges(40,51))
+##      #print( paste("Query: ", codingRange@start, " and ", (codingRange@start + codingRange@width - 1), " is within the coding sequence.", sep=''))
+##      if (featureIsEntirelyBetween(queryRange, codingRange)) {
+##           xPrintRow(toupper(rowItem[3]), queryRange, rowItem)
+##      } else if (featureIsEntirelyOutside(queryRange, codingRange)) {
+##           # Any exons BEFORE the start codon are 5' UTR, and any exons AFTER the stop codon are 3' UTR
+##           isFivePrimeUtr = (isPositiveStrand && queryRange@start < codingRange@start) || (!isPositiveStrand && queryRange@start > codingRange@start)               # it's either  HERE >>>>>>>>>>>>>>>>>> (on the positive strand)   or                <<<<<<<<<<<<<<<<<<<<<<< HERE (on the negative strand)
+##           isThreePrimeUtr = (!isPositiveStrand && queryRange@start < codingRange@start) || (isPositiveStrand && queryRange@start > codingRange@start)               # it's either       HERE <<<<<<<<<<<<<<<<<<<<<<<< (negative strand) or                           >>>>>>>>>>>>>>>>>> HERE   (positive strand)
+##           stopifnot(isFivePrimeUtr + isThreePrimeUtr == 1) # EXACTLY one of the two conditions must be true, or there's something wrong in how I programmed this!
+##           utrTypeString <- ifelse(isFivePrimeUtr, yes="UTR_FIVE_PRIME", no="UTR_THREE_PRIME")
+##           xPrintRowRaw(utrTypeString, queryRange, rowItem)
+##      } else {
+##           #print(paste("Partially inside at ", rangeStr(queryRange), " ****", sep=''))
+##           queryCodeOnly  <- IRanges::intersect(queryRange, codingRange) # only the part of the query that is ALSO in the coding region
+##           queryUtrOnly   <-   IRanges::setdiff(queryRange, codingRange) # subtract out the coding part from the query
+##           xPrintRowExceptStartStop("EXON_CODING_ONLY",   queryCodeOnly, rowItem) # only the coding part!
+##           xPrintRowExceptStartStop("UTR_SECTION_OF_EXON", queryUtrOnly, rowItem) # only the NON-CODING part
+##           xPrintRow(paste(toupper(rowItem[3]), "_INCLUDING_UTR", sep=''), queryRange, rowItem) # the exon with both coding AND non-coding parts
+##      }
+## }
+
+
 
 
 startTime = date() # save the time when we started
 
-
-
+BIGGEST_POSSIBLE_GENOMIC_COORDINATE <- (.Machine$integer.max - 100) # This needs to be greater than any valid genomic coordinate, but not so big as to instantly cause integer overflow. What a mess. Why did I use IRanges anyway!!!
 NUM_INTERESTING_ITEMS_IN_RESULT_VEC <- 5
+
 resMat <- matrix("", nrow=nrow(tomato), ncol=NUM_INTERESTING_ITEMS_IN_RESULT_VEC)
 for (i in seq_along(column9)) {
      resMat[i, 1:NUM_INTERESTING_ITEMS_IN_RESULT_VEC] <- c("gene_id"        = devour("gene_id "      , column9[[i]])
@@ -135,22 +168,12 @@ ddd <- cbind( tomato[ , INTERESTING_ORIGINAL_COLUMNS], resMat) # smash them toge
 colnames(ddd) <- c("CHR","ABOUT","TYPE","START","STOP","STRAND","GENE","TS","EXON","EXNUM","COMMON")
 
 uniqTS.vec <- as.character(unique(ddd$TS)) # unique transcripts only (as characters)
-firstUniqueTranscript <- uniqTS.vec[1]
 
 for (ut in uniqTS.vec) {
-     a.frame <- ddd[ ddd$TS == ut, , drop=F]
-     b.frame <- a.frame[ order(a.frame$START), ] # re-order the data frame by start positions of each exon/element! It should already be in that order, but just in case...
-     ec.frame <- b.frame[ b.frame$TYPE %in% c("exon", "CDS"), c("START","STOP")] # exons and CDs
-     #print("Got the 'ec.frame', which contains the start and stop locations for all EXON and CDS elements in this transcript.")
-
+     b.frame <- ddd[ ddd$TS == ut, , drop=F] # we dont' actually even care if the transcripts are presented in order, although it's nice if they are
+     ec.frame <- b.frame[ b.frame$TYPE %in% c("exon", "CDS"), c("START","STOP")] # exons and CDs #print("Got the 'ec.frame', which contains the start and stop locations for all EXON and CDS elements in this transcript.")
      tsFullRange <- IRanges(start=min(b.frame$START, b.frame$STOP), end=max(b.frame$START, b.frame$STOP)) # Start it at the FULL transcript range -- min to max
-     intronRanges <- tsFullRange
-     for (i in seq_along(rownames(ec.frame))) {
-          # All regions that are 1) NOT in exons
-          #              and are 2) between the first and last exons
-          #                         are introns.
-          intronRanges <- setdiff(intronRanges, IRanges(start=ec.frame[i, "START"], end=ec.frame[i, "STOP"])) # Now SUBTRACT OUT each "exon or CDS" IRange, leaving only things that were NOT in an exon and were NOT in a CDS
-     }
+     intronRanges <- setdiff(tsFullRange, IRanges(start=ec.frame$START, end=ec.frame$STOP))
      
      nStartCodons = sum(b.frame$TYPE == "start_codon")
      nStopCodons  = sum(b.frame$TYPE ==  "stop_codon")
@@ -161,14 +184,10 @@ for (ut in uniqTS.vec) {
 
      #print(paste("Strand: ", strand, sep=''))
      #xprint("Transcript <", ut, "> had ", nStartCodons, " start codons (", rep("S", times=nStartCodons),  ") and ", nStopCodons, " stop codons (", rep("E", times=nStopCodons), ").")
-
      if (nStartCodons != nStopCodons) {
           #print("WARNING: UNEQUAL NUMBER OF START & STOP CODONS. This appears to be a common situation, so I guess it is by design?")
           #stopifnot(nStartCodons == nStopCodons)
      }
-     
-     
-     BIGGEST_POSSIBLE_GENOMIC_COORDINATE <- (.Machine$integer.max - 100) # This needs to be greater than any valid genomic coordinate, but not so big as to instantly cause integer overflow. What a mess. Why did I use IRanges anyway!!!
      
      codingLimitLeft  <- -1 # <-- can be any out-of-bounds invalid genomic coordinate
      codingLimitRight <- (codingLimitLeft-1) # one less than codingLimitLeft, so that IRanges is happy. CANNOT be more than one unit less!
@@ -193,14 +212,8 @@ for (ut in uniqTS.vec) {
                xPrintRow(toupper(rowItem$TYPE), queryRange, rowItem)
           } else if (featureIsEntirelyOutside(queryRange, codingRange)) {
                # Any exons BEFORE the start codon are 5' UTR, and any exons AFTER the stop codon are 3' UTR
-               # Note that we just check the relative positions of the queryRange start and codingRange start; we don't actually bother to figure out the widths, because we don't care---
-               # we ALREADY know that the queryRange is totally outside of the codingRange
-               isFivePrimeUtr = (isPositiveStrand && queryRange@start < codingRange@start) || (!isPositiveStrand && queryRange@start > codingRange@start)
-               # it's either  HERE >>>>>>>>>>>>>>>>>> (on the positive strand)
-               # or                <<<<<<<<<<<<<<<<<<<<<<< HERE (on the negative strand)
-               isThreePrimeUtr = (!isPositiveStrand && queryRange@start < codingRange@start) || (isPositiveStrand && queryRange@start > codingRange@start)
-               # it's either       HERE <<<<<<<<<<<<<<<<<<<<<<<< (negative strand)
-               # or                           >>>>>>>>>>>>>>>>>> HERE   (positive strand)
+               isFivePrimeUtr = (isPositiveStrand && queryRange@start < codingRange@start) || (!isPositiveStrand && queryRange@start > codingRange@start)               # it's either  HERE >>>>>>>>>>>>>>>>>> (on the positive strand)   or                <<<<<<<<<<<<<<<<<<<<<<< HERE (on the negative strand)
+               isThreePrimeUtr = (!isPositiveStrand && queryRange@start < codingRange@start) || (isPositiveStrand && queryRange@start > codingRange@start)               # it's either       HERE <<<<<<<<<<<<<<<<<<<<<<<< (negative strand) or                           >>>>>>>>>>>>>>>>>> HERE   (positive strand)
                stopifnot(isFivePrimeUtr + isThreePrimeUtr == 1) # EXACTLY one of the two conditions must be true, or there's something wrong in how I programmed this!
                utrTypeString <- ifelse(isFivePrimeUtr, yes="UTR_FIVE_PRIME", no="UTR_THREE_PRIME")
                xPrintRowRaw(utrTypeString, queryRange, rowItem)
@@ -214,6 +227,8 @@ for (ut in uniqTS.vec) {
           }
      }
 
+     #apply(b.frame, 1, handleARow)
+     
      for (i in seq_along(intronRanges)) {
           theIntronRange <- intronRanges[i]
           xPrintRowRaw("INTRON", theIntronRange, b.frame[1,])
