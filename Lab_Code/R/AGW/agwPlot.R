995e01ded7378b989dc766e05f38912e40ed5e5e
alexgw <alexgw@localhost>
2013-05-08 22:47:02 -0700

    Commit by alexgw

--- Lab_Code/R/AGW/agwPlot.R
+++ Lab_Code/R/AGW/agwPlot.R
@@ -466,7 +466,9 @@ pairsCorMatrixPlotAGW <- function(filePath, dataMatrix, labelVec=NULL, keys, mai
      labelVecNoNewlines <- gsub("\n", "", labelVec, perl=TRUE, ignore.case=TRUE)
      print.agw("Drawing a \"pairs\" plot matrix with many sub-plots to file <", filePath, ">... (note, this can take a couple of minutes)\n")
      ## cumul/whichBin: Shows which "bins" each number is in. So like, experiment #2 might be in experimental group #1, if there were 2 replicates of that experiment.
-     cumul <- c(0, cumsum(keys$"experimentsInEachGroup")) ## <-- 0 is the leftmost boundary!
+
+     exprs.in.each.group <- table(keys$group.assignments)
+     cumul <- c(0, cumsum(exprs.in.each.group)) ## <-- 0 is the leftmost boundary!
      whichBin <- function(value, binMarkers) { ## right sides! The first value is NOT a valid one
           ## Tells you which "bin" an array is in. The bin is the experimental group.
           ## So if you have 9 arrays, in 3 groups (control, control, control, exp1, exp1, exp1, exp2, exp2, exp2),
