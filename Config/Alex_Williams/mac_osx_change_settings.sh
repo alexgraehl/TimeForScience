a5fc385d63894d685628065c469db3448f784e2e
alexgw <alexgw@localhost>
2013-11-18 16:05:31 -0800

    Commit by alexgw

--- Config/Alex_Williams/mac_osx_change_settings.sh
+++ Config/Alex_Williams/mac_osx_change_settings.sh
@@ -3,10 +3,10 @@
 # ~/.osx — http://mths.be/osx
 
 # Ask for the administrator password upfront
-sudo -v
+#A#sudo -v
 
 # Keep-alive: update existing `sudo` time stamp until `.osx` has finished
-while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+#A#while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 ###############################################################################
 # General UI/UX                                                               #
@@ -22,7 +22,7 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 #sudo pmset -a standbydelay 86400
 
 # Disable the sound effects on boot
-#sudo nvram SystemAudioVolume=" "
+#A#sudo nvram SystemAudioVolume=" "
 
 # Menu bar: disable transparency
 defaults write NSGlobalDomain AppleEnableMenuBarTransparency -bool false
