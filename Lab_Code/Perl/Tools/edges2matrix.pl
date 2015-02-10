af4ad929a89675ee05a0f27c8928c12981565721
alexgw <alexgw@localhost>
2015-02-10 00:50:03 -0800

    Commit by alexgw

--- Lab_Code/Perl/Tools/edges2matrix.pl
+++ Lab_Code/Perl/Tools/edges2matrix.pl
@@ -35,6 +35,42 @@ use lib "$ENV{MYPERLDIR}/lib"; use lib "$ENV{TIME_FOR_SCIENCE_DIR}/Lab_Code/Perl
 use strict;
 use warnings;
 
+print STDERR "Note: sets.pl does something similar to what this script does, but with more options!";
+
+
+
+# If you feed this program two columns, and it gives you a matrix.
+# For example, if you have a list of gene interactions of some kind,
+# it prints out "1" when the interaction is in the list, and "0" otherwise.
+
+
+# If you feed this program THREE columns, it gives you a matrix with the cell value being whatever was in the third column.
+# For example, if you have a list of gene interactions of some kind,
+# it prints out "1" when the interaction is in the list, and "0" otherwise.
+
+
+# There are TWO MODES for this:
+
+# Give it TWO columns,:
+# A  B
+# A  C
+# A  D
+# B  C
+# D  E
+
+# And it will give you back:
+#    A  B  D
+# B  1  0  0
+# C  1  1  0
+# D  0  0  0
+# E  0  0  1
+
+# If you give it THREE columns instead, then it gives you the value in the third column!
+
+
+
+
+
 # Flush output to STDOUT immediately.
 $| = 1;
 
@@ -42,6 +78,7 @@ my @flags   = (
                   [    '-q', 'scalar',     0,     1]
                 , [   '-k1', 'scalar',     1, undef]
                 , [   '-k2', 'scalar',     2, undef]
+                , [   '-v',  'scalar',     3, undef]
                 , [    '-d', 'scalar',  "\t", undef]
                 , [    '-s', 'scalar',     0,     1]
                 , ['--file', 'scalar',   '-', undef]
@@ -58,6 +95,7 @@ if(exists($args{'--help'}))
 my $verbose   = not($args{'-q'});
 my $key_col1  = $args{'-k1'};
 my $key_col2  = $args{'-k2'};
+my $val_col   = $args{'-v'};
 my $delim     = $args{'-d'};
 my $symmetric = $args{'-s'};
 my $file      = $args{'--file'};
@@ -89,7 +127,6 @@ exit(0);
 
 sub addEntry {
    my ($sets, $from, $to) = @_;
-
    if(not(exists($$sets{$from}))) {
       my %set;
       $$sets{$from} = \%set;
