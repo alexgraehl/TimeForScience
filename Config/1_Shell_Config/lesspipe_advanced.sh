29ff7644ab81c6891739389e29efc48cc60565d6
alexgraehl <alexgraehl@users.noreply.github.com>
2015-04-25 14:21:31 -0700

    Committing any local changes from alexgw before sync with remote server.

--- Config/1_Shell_Config/lesspipe_advanced.sh
+++ Config/1_Shell_Config/lesspipe_advanced.sh
@@ -22,23 +22,19 @@ smartdecompress() {
     $DECOMPRESSOR $1; # <-- this is what we pass into the relevant "smart" function below
 }
 
-STO="\033[1m" # start color... not useful?
-STA="\033[31m" # start color A: 31 = red
-STC="\033[36m" # start color C: 36 = cyan. 32 = green. 35 = magenta
-STG="\033[33m" # start color G: 33 = yellow
-STT="\033[35m" # start color T: 36 = cyan. 35 = magenta
-STN="\033[45m" # start color N: magenta background, doesn't change the foreground
-
-RES="\033[0m" # RESET color
-
+#STO="\033[1m" # start color... not useful?
+#STA="\033[31m" # start color A: 31 = red
+#STC="\033[36m" # start color C: 36 = cyan. 32 = green. 35 = magenta
+#STG="\033[33m" # start color G: 33 = yellow
+#STT="\033[35m" # start color T: 36 = cyan. 35 = magenta
+#STN="\033[45m" # start color N: magenta background, doesn't change the foreground
+#RES="\033[0m" # RESET color
 basecolor() {
     ## Colors the A, C, G, and T
     ## Assumes that you pass something INTO it via a pipe -- otherwise it fails
     ## Example:  cat myfile | sed 's/1/2/' | basecolor 
     #sed -e 's/\(AA*\)/\1'"$(printf ${STA}A${RES})"'/g' -e 's/C/'"$(printf ${STC}C${RES})"'/g' -e 's/G/'"$(printf ${STG}G${RES})"'/g' -e 's/T/'"$(printf ${STT}T${RES})"'/g' -e 's/N/'"$(printf ${STN}N${RES})"'/g'
-
     perl ${TIME_FOR_SCIENCE_DIR}/Config/1_Shell_Config/lesspipe_extra_bamfile_coloration.pl
-
     #sed -e 's/A/'"$(printf ${STA}A${RES})"'/g' -e 's/C/'"$(printf ${STC}C${RES})"'/g' -e 's/G/'"$(printf ${STG}G${RES})"'/g' -e 's/T/'"$(printf ${STT}T${RES})"'/g' -e 's/N/'"$(printf ${STN}N${RES})"'/g'
 
     # Colorize based on entire blocks of identical characters. Turns out not to be any faster, unfortunately!
@@ -63,6 +59,10 @@ lesspipe() {
 		$DECOMPRESSOR -- "$1"
 	    fi ;; ## <-- Done with the "multi-part archive" part
 
+	
+
+	
+
   # Alex:
   # CHANGED from default lesspipe: Now we load any files ending in
   # .tab or .matrix (including gzipped files) and run them through
@@ -72,16 +72,15 @@ lesspipe() {
   # For a faster but less feature-filled
   #  UNIX-tool-only version of this, instead of "sheet.pl", use "column -t -s '	'"
 	*.tab.gz|*.matrix.gz) gunzip -c "$1" | sheet.pl --notify --color="always" ;;
-	*.tab|*.matrix) sheet.pl --notify --color="always" "$1" ;;
-
-	# Color code the bases A, C, G, and T
-	*.bam) echo -e '##################\n# Viewing a BAM file with "samtools view -h" -- The actual file is in a binary format #\n##################' ; samtools view -h "$1" | basecolor ;; ## Use Samtools to view a BAM ("binary sam") RNA-sequence file
-
-	# Color code the bases A, C, G, and T
+	*.tab|*.matrix) sheet.pl --notify --color="always" "$1" ;; # Maybe should use sheet.py instead?
+	
+	*.bam) if [ -x "`which samtools`"]; then  # Color code the bases A, C, G, and T (and N)
+		  echo -e '##################\n# Viewing a BAM file with "samtools view -h" -- The actual file is in a binary format #\n##################' ; samtools view -h "$1" | basecolor ## Use Samtools to view a BAM ("binary sam") RNA-seq file
+	      else  ## Use Samtools to view a BAM ("binary sam") RNA-sequence file
+		  echo -e "ERROR: LESSPIPE_ADVANCED CANNOT VIEW THIS FILE, BECAUSE 'samtools' IS NOT INSTALLED or CANNOT BE FOUND.\nPlease install samtools (or make sure it is on your path and can be executed) and try again!"
+	       fi ;;
+	###########	*.bam) echo '######\n### Viewing a BAM file with "samtools view -h" -- The actual file is in a binary format ###\n######' ; samtools view -h "$1" | sed 's/A/'"$(printf '\033[1mA\033[0m')"'/g' ;; ## Use Samtools to view a BAM ("binary sam") RNA-sequence file
 	*.sam|*.sam.gz|*.sam.bz2|*.fasta|*.fasta.gz|*.fasta.bz2|*.fastq|*.fastq.gz|*.fastq.bz2|*.fa|*.fa.gz|*.fa.bz2|*.fq|*.fq.gz|*.fq.bz2) smartdecompress "$1" | basecolor ;; ## Use Samtools to view a BAM ("binary sam") RNA-sequence file
-
-###########	*.bam) echo '######\n### Viewing a BAM file with "samtools view -h" -- The actual file is in a binary format ###\n######' ; samtools view -h "$1" | sed 's/A/'"$(printf '\033[1mA\033[0m')"'/g' ;; ## Use Samtools to view a BAM ("binary sam") RNA-sequence file
-
 	*.tar) tar tvvf "$1" ;;
 	*.tgz|*.tar.gz|*.tar.[zZ]) tar tzvvf "$1" ;;
 	*.tar.bz2|*.tbz2) bzip2 -dc "$1" | tar tvvf - ;;
--- Config/1_Shell_Config/lesspipe_extra_bamfile_coloration.pl
+++ Config/1_Shell_Config/lesspipe_extra_bamfile_coloration.pl
@@ -2,31 +2,54 @@
 use strict;
 use warnings;
 
-my $STO=qq{\033[1m} ; # start color
+# BEWARE WHEN CHANGING THIS PROGRAM:
+# Note: if you make any errors in this file, "less" will stop coloring fastq files WITHOUT ANY ERROR REPORTING.
+# You will have to actually run 'perl -c' with this program to check it, or better yet, actually debug it with real input files.
 
-my $CCC=qq{\033[44m\033[1;37m}; # colon delimiter color; 34 = blue
+my $STO=qq{\033[37m} ; # "default" white color. Assumes dark terminal!
+my $CCC=qq{\033[44m\033[1;37m}; # colon delimiter color; 34 = blue (NOT USED RIGHT NOW)
 
-my $STA=qq{\033[32m}; # start color A: 32 = green   31 = red  34 = blue
-my $STT=qq{\033[31m}; # start color T: 36 = cyan. 35 = magenta
-my $STC=qq{\033[36m}; # start color C: 36 = cyan; 31 = red, 36 = cyan. 32 = green. 35 = magenta
-my $STG=qq{\033[33m}; # start color G: 33 = yellow
+# The most common standard for colors (in sequence logos) is: A = green, C = blue, T = red, G = yellow
+
+# F_COLOR = FOREGROUND IS THIS COLOR
+my $F_RED    = qq{\033[31m};
+my $F_GREEN  = qq{\033[32m};
+my $F_YELLOW = qq{\033[33m};
+my $F_CYAN   = qq{\033[36m};
+
+my $STA=$F_GREEN; # start color A: 32 = green 31 = red  34 = blue
+my $STT=$F_RED; # start color T: 36 = cyan. 35 = magenta
+my $STC=$F_CYAN; # start color C: 36 = cyan; 31 = red, 36 = cyan. 32 = green. 35 = magenta
+my $STG=$F_YELLOW; # start color G: 33 = yellow
+
+# Coloring the BACKGROUND instead of the foreground (this sounds like it would work well, but it looks really ugly!)
+#my $STA=qq{\033[42m}; # start color A: 32 = green 31 = red  34 = blue
+#my $STT=qq{\033[41m}; # start color T: 36 = cyan. 35 = magenta
+#my $STC=qq{\033[44m}; # start color C: 36 = cyan; 31 = red, 36 = cyan. 32 = green. 35 = magenta
+#my $STG=qq{\033[43m}; #  \033[45m}; # start color N: magenta background, black foreground
+
+# Note: currently we assume that ONLY THE 'N' has a background
+# color. If that is not true, then we need to change the
+# line with /[Nn]/ to push the 'reset' color for other bases
+# with a background color. Otherwise the background "leaks"
+# beyond where it is supposed to be.
 
 my $STN=qq{\033[45m\033[1;37m}; #  \033[45m}; # start color N: magenta background, black foreground
 my $RES=qq{\033[0m} ; # RESET color
 
-my $MIN_DNA_SEQ_LENGTH_TO_COLOR = 5; # Color a run of ACGTN if they are at least THIS long (or longer).
-
+my $MIN_DNA_SEQ_LENGTH_TO_COLOR = 5; # Color a run of "ACGTN-" if they are at least THIS long (or longer).
 
 my %colors = ( 'A' => $STA
-		   , 'a' => $STA
-		   , 'T' => $STT
-		   , 't' => $STT
-		   , 'G' => $STG
-		   , 'g' => $STG
-		   , 'C' => $STC
-		   , 'c' => $STC
-		   , 'N' => $STN
-		   , 'n' => $STN
+	       , 'a' => $STA
+	       , 'T' => $STT
+	       , 't' => $STT
+	       , 'G' => $STG
+	       , 'g' => $STG
+	       , 'C' => $STC
+	       , 'c' => $STC
+	       , 'N' => $STN
+	       , '-' => $RES # Hyphen does not get colored--reset it to the terminal default!
+	       
 );
 
 while (my $line = <>) {
@@ -38,7 +61,9 @@ while (my $line = <>) {
 
     my $prevC = '';
     for my $c (@arr) {
-	if ($c =~ /[ACGTNacgtn]/) {
+	if ($c =~ /[-ACGTNacgtn]/) {
+	    # Hyphen (gap) is allowed to be "maybe" part of a DNA sequence. It gets whatever color the previous base had, if any.
+	    # We could fix this by resetting the color after every base, if we really wanted to.
 	    $inDNA = 1;
 
 	    my $toPush;
@@ -47,21 +72,20 @@ while (my $line = <>) {
 	    } else {
 		$toPush = $colors{$c} . $c;
 	    }
-	    if ($c =~ /[Nn]/) { $toPush .= ${RES}; } # note that we add the $RES reset character AFTER AN 'N' but not anything else. That's because N needs its background reset, as it is the only letter with a colored background.
+	    if ($c =~ /[Nn]/) { $toPush .= $RES; } # note that we add the $RES reset character AFTER AN 'N' but not anything else. That's because N needs its background reset, as it is the only letter with a colored background.
 	    
 	    push(@potentiallyInteresting, $toPush);
 	    push(@boringVersion, $c);
 	} else {
 	    if ($inDNA) {
 		if (scalar(@potentiallyInteresting) >= $MIN_DNA_SEQ_LENGTH_TO_COLOR) {
-		    print join('',@potentiallyInteresting) . ${RES};
+		    print join('',@potentiallyInteresting) . $RES;
 		} else {
 		    print join('',@boringVersion);
 		}
 		@potentiallyInteresting = @boringVersion = (); # clear it!
 		$inDNA = 0;
 	    }
-	    
 	    #if ($c eq ':') { print "$CCC:$RES"; }
 	    #else { print $c; }
 	    print $c;
