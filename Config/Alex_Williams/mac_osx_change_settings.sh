c1aeef8de3a07395b29eca577cade469a3eeb061
alexgraehl <alexgraehl@users.noreply.github.com>
2015-06-10 00:24:56 -0700

    Committing any local changes from alexgw before sync with remote server.

--- Config/Alex_Williams/mac_osx_change_settings.sh
+++ Config/Alex_Williams/mac_osx_change_settings.sh
@@ -5,8 +5,11 @@
 # Ask for the administrator password upfront
 sudo -v
 
+echo "Note: this will take about 60-90 seconds--don't stop it early... let it run to completion."
 # Keep-alive: update existing `sudo` time stamp until `.osx` has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+echo "Done with the extra-slow step..."
 
 ###############################################################################
 # General UI/UX                                                               #
