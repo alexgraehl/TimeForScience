9ad6a55ef9038f8fcdca4b0c36604b5b779cdb5b
alexgraehl <alexgraehl@users.noreply.github.com>
2016-05-19 18:27:27 -0700

    Committing any local changes from alexgw before sync with remote server.

--- Lab_Code/Perl/Tools/fasta2gtf_one_line_per_chr.pl
+++ Lab_Code/Perl/Tools/fasta2gtf_one_line_per_chr.pl
@@ -10,8 +10,6 @@ use Getopt::Long;
 use Carp; # backtrace on errors. Has the "confess" function. Use this instead of "die" if you want useful information!   
 
 
-
-
 sub main();
 sub quitWithUsageError($) { print($_[0] . "\n"); printUsageAndQuit(); print($_[0] . "\n"); }
 sub printUsageAndQuit() { printUsage(); exit(1); }
@@ -20,8 +18,6 @@ sub printUsage() {
     print STDOUT <DATA>;
     exit(0);
 }
-
-
 
 
 my $feature = undef;
@@ -35,7 +31,7 @@ sub printFeatAsGtfLine($$) {
 	my $start = 1; # ("Start position of the feature, with sequence numbering starting at 1.": http://uswest.ensembl.org/info/website/upload/gff.html)
 	my $end   = $start+$feat_len-1; # We do NOT add one to the length---in fact, we SUBTRACT one, because "1 to 1" is length 1, "1 to 2" is length 2, etc.  "<start> must be less than or equal to <end>"
 	if ($end < $start) { confess "End cannot be less than start. Feature length must have been 0, which should be impossible."; }
-	my $score = "0.00";
+	my $score = "."; # '.' = no relevant score
 	my $strand = "+";
 	# From GTF docs: http://mblab.wustl.edu/GTF22.html
 	# The following feature types are required: "CDS", "start_codon", "stop_codon". The features "5UTR", "3UTR", "inter", "inter_CNS", "intron_CNS" and "exon" are optional. All other features will be ignored. The types must have the correct capitalization shown here.
@@ -49,10 +45,8 @@ sub printFeatAsGtfLine($$) {
 
 sub main() {
 	$Getopt::Long::passthrough = 1; # ignore arguments we don't recognize in GetOptions, and put them in @ARGV
-
 	GetOptions("help|?|man" => sub { printUsageAndQuit(); }
 		  ) or printUsageAndQuit();
-
 	my %feats = ();		# key = feature, value = length
 	my $curFeature = undef;
 	my $lineNum = 0;
@@ -65,17 +59,16 @@ sub main() {
 			if (exists($feats{$curFeature})) {
 				confess "ERROR: Duplicate FASTA entries! There were multiple lines with the 'name' field (the part after the '>') as <$curFeature>, which is forbidden! Each FASTA name must be UNIQUE.";
 			}
-			$feats{$curFeature} = 0	# New feature found! $feature = $1;
-			  #$len = 0; # reset it to zero!
+			$feats{$curFeature} = 0;	# New feature found! $feature = $1;
+			#$len = 0; # reset it to zero!
 		} else {
 			if (!defined($curFeature)) {
 				confess "ERROR: Odd... found something that is NOT in a feature, on line $lineNum...\n";
 			}
-			$line =~ s/\s//g; # remove **all** whitespace from this line!
+			$line =~ s/\s//g; # remove **all** whitespace from this line, before we compute its length.
 			$feats{$curFeature} += length($line);
 		}
 	}
-
 	foreach my $k (sort(keys(%feats))) { 
 		printFeatAsGtfLine($k, $feats{$k});
 	}
