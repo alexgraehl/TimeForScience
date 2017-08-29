0e6737f9ffad675b0aae37e26295e748833ff382
Alex Williams <alex.williams@epibiome.com>
2017-08-29 15:26:45 -0700

    Committing any local changes from alexwilliams before sync with remote server.

--- Lab_Code/Perl/Tools/transpose.pl
+++ Lab_Code/Perl/Tools/transpose.pl
@@ -1,114 +1,60 @@
 #!/usr/bin/perl
 
-#@COMMENT@ transpose.pl will transpose a tabular data matrix (it flips the rows/columns). This is otherwise surprisingly convoluted to accomplish. Frequency-of-use rating: 10/10.
-
-
-##############################################################################
-##############################################################################
-##
-## transpose.pl
-##
-##############################################################################
-##############################################################################
-##
-## Written by Josh Stuart in the lab of Stuart Kim, Stanford University.
-##
-##  Email address: jstuart@stanford.edu
-##          Phone: (650) 725-7612
-##
-## Postal address: Department of Developmental Biology
-##                 Beckman Center Room B314
-##                 279 Campus Dr.
-##                 Stanford, CA 94305
-##
-##       Web site: http://www.smi.stanford.edu/people/stuart
-##
-##############################################################################
-##############################################################################
-##
-## Written: 00/00/02
-## Updated: 00/00/02
-##
-##############################################################################
-##############################################################################
-
-use lib "$ENV{MYPERLDIR}/lib"; use lib "$ENV{TIME_FOR_SCIENCE_DIR}/Lab_Code/Perl/LabLibraries"; require "libfile.pl";
-
-use strict;
-use warnings;
-
-#$| = 1; # Actually no need to flush output to STDOUT immediately. Note: STDERR is still not synchronized with this, and STDERR output may be interleaved
-
-my @flags   = (
-    [    '-q', 'scalar',     0,     1]
-    , [    '-d', 'scalar',  "\t", undef]
-    , ['--file', 'scalar',   '-', undef]
-    );
-
-my %args = %{&parseArgs(\@ARGV, \@flags)};
-
-if(exists($args{'--help'})) {
-    print STDOUT <DATA>;
-    exit(0);
-}
-
-my $verbose = not($args{'-q'});
-my $delim   = $args{'-d'};
-my $file    = $args{'--file'};
-my $r       = 0;
-my $c       = 0;
-my $ncols   = -1;
+#@COMMENT@ transpose.pl transposes a table of data (flips the rows/columns). Example:  cat myfile.txt | transpose.pl > flipped.txt. Frequency-of-use rating: 10/10.
+use strict; use warnings; use Getopt::Long;
+my $verbose = 0;
+my $delim   = "\t";
+$Getopt::Long::passthrough = 1; # ignore arguments we don't recognize in GetOptions, and put them in @ARGV
+GetOptions("help|?|man"   => sub { print STDOUT <DATA>;exit(0); }
+	   , "delim|d=s"  => \$delim
+	   , "v|verbose!" => \$verbose
+	  ) or do { print STDOUT <DATA>;exit(1); };
+my $ncols = -1;
 my @x;
-
-$verbose and print STDERR "Reading in table from '$file'...\n";
-my $filep = &openFile($file);
-while(<$filep>) {
-    my @row = split($delim);
+#sub max($$) { $_[0] > $_[1] ? $_[0] : $_[1]; } # return max
+my $r     = 0; # save this outside the loop below!!!
+foreach my $line (<>) { # <-- read from STDOUT or from a passed-in file
+    my @row = split($delim, $line);
     chomp($row[$#row]);
-    for($c = 0; scalar(@row) > 0; $c++)
-    {
-	$x[$r][$c] = shift @row;
+    my $c = 0;
+    for ($c = 0; scalar(@row) > 0; $c++) {
+	    $x[$r][$c] = shift(@row);
     }
-    if($c > $ncols)
-    {
-	$ncols = $c;
-    }
+    if ($c > $ncols) { $ncols = $c; }
     $r++;
 }
-close($filep);
-
 my $nrows = $r;
-
 $verbose and print STDERR " done ($nrows by $ncols)!\n";
 $verbose and print STDERR "Transposing to $ncols by $nrows...\n";
-for($c = 0; $c < $ncols; $c++) {
-    for($r = 0; $r < $nrows; $r++) {
-	my $x = defined($x[$r][$c]) ? $x[$r][$c] : '';
+for(my $cc = 0; $cc < $ncols; $cc++) {
+    for(my $rr = 0; $rr < $nrows; $rr++) {
+	my $x = defined($x[$rr][$cc]) ? $x[$rr][$cc] : '';
 	print "$x";
 	if ($r < $nrows - 1) { print $delim; }
     }
     print "\n";
 }
-
 exit(0);
-
 __DATA__
-syntax: transpose.pl [OPTIONS] [FILE | < FILE]
+----------------------------------------------------------------
+transpose.pl [-d DELIM] [FILE | < FILE]
+----------------------------------------------------------------
+Transposes a table -- flips the rows and columns.
+The original file is assumed to have rows delimited by newlines
+and columns delimited by tabs.
+Possibly requires UNIX-format newlines (\n).
 
-Transposes a table -- flips the rows and columns so that what
-were columns in the original table become the rows and what
-were rows in the original table become the columns.  The original
-file is assumed to have rows delimited by newlines and columns
-delimited by tabs (the column delimiter can actually be set with
-the -d flag; see below).
+FILE: a file containing a tabular data plain text file.
+   Example:      MyHeader      Col2     Col3
+                 Row1_yes      a        b
+                 Row2_maybe    c        d
 
-FILE: a file containing a table with each row containing a tuple
-  and each field in the tuple seperated by a delimiter.  By
-  default, the delimiter is assumed to be tab.
+Example usage:     cat myfile.tab.txt | transpose.pl > out.txt
+		 transpose.pl -d ',' file.csv > transposed.csv
 
-OPTIONS are:
--d DELIM: set the delimiter for the columns to DELIM (default is
-          tab).
+OPTIONS:
+   -d DELIM        : Change column delimiter (default: tab).
+   or --delim=DELIM: Example: --delim=',' for a CSV file.
 
-
-
+   -v      : More verbose output to STDERR
+================================================================
