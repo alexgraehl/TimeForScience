e47d1c05df0d718fdee8c1dc154f061877430e62
Alex Williams <alexgraehl@gmail.com>
2017-08-31 00:55:21 -0700

    Committing any local changes from alexgw before sync with remote server.

--- Lab_Code/Perl/Tools/transpose.pl
+++ Lab_Code/Perl/Tools/transpose.pl
@@ -1,8 +1,8 @@
 #!/usr/bin/perl -w
 #@COMMENT@ transpose.pl transposes a table of data (flips the rows/columns). Example:  cat myfile.txt | transpose.pl > flipped.txt. Frequency-of-use rating: 10/10.
+# Original version by Josh Stuart, 2000. Modified by Alex Williams, 2017.
 use strict; use warnings; use Getopt::Long;
-my ($verbose, $delim) = (0, "\t");
-#echo -e "\n\n\na\nb\n1,2,3,4,5,6\na,b,c,d,e,f\ng,h,i\n,,z"
+my ($verbose, $delim) = (0, "\t");    #echo -e "\n\n\na\nb\n1,2,3,4,5,6\na,b,c,d,e,f\ng,h,i\n,,z"
 $Getopt::Long::passthrough = 1; # ignore arguments we don't recognize in GetOptions, and put them in @ARGV
 GetOptions("help|?|man"   => sub { print STDOUT <DATA>;exit(0); }
 	   , "delim|d=s"  => \$delim
@@ -31,13 +31,13 @@ exit(0);
 __DATA__
 transpose.pl [-d DELIM] [FILE | < FILE]
 
-Transposes a table -- flips the rows and columns.
-The original file is assumed to have rows delimited by newlines
+Transposes a table -- flips the rows and columns, writes to STDOUT.
+The input file/STDIN is assumed to have rows delimited by newlines
 and columns delimited by tabs.
 Possibly requires UNIX-format newlines (\n).
 
 FILE: a file containing a tabular data plain text file.
-   Example:      MyHeader      Col2     Col3
+   e.g.:         MyHeader      Col2     Col3
                  Row1_yes      a        b
                  Row2_maybe    c        d
 
@@ -47,6 +47,5 @@ Example usage:     cat myfile.tab.txt | transpose.pl > out.txt
 OPTIONS:
    -d DELIM        : Change column delimiter (default: tab).
    or --delim=DELIM: Example: --delim=',' for a CSV file.
-
    -v      : More verbose output to STDERR
 ================================================================
