d9fca5517f9542bfedd7128fa3e6791933379841
Alex Williams <alex.williams@epibiome.com>
2017-12-13 13:41:40 -0800

    Committing any local changes from alexwilliams before sync with remote server.

--- Config/Alex_Williams/mac_install_software.sh
+++ Config/Alex_Williams/mac_install_software.sh
@@ -42,12 +42,10 @@ echo "install iTerm2 from ____________"
 echo "Sync the iTerm2 preferences from my config folder on github"
 echo "install OmniGraffle (Paid!) from ____________"
 echo "install Transmit (Paid!) (File transfer) from ____________"
-echo "install Evernote from ____________"
-echo "install Sourcetree (git/mercurial) from ____________"
-echo "install Hermes (Pandora) from ____________"
+echo "install Hermes (Pandora):"
+# curl https://github.com/HermesApp/Hermes/archive/master.zip > ~/Downloads/hermes_pandora_app.zip
 echo "install Clipy from https://clipy-app.com/ (successor to ClipMenu)"
-
-echo "Install XCode"
+echo "Install XCode from the app store"
 echo "Run XCode to let it install its weird components"
 echo "maybe something like this: sudo /Applications/Xcode.app/Contents/Developer/usr/bin/xcodebuild  -license accept"
 #sudo installer -pkg /Applications/Xcode-beta.app/Contents/Resources/Packages/MobileDevice.pkg -target /
@@ -57,16 +55,15 @@ echo "maybe something like this: sudo /Applications/Xcode.app/Contents/Developer
 #                         Homebrew
 # ==============================================================================
 
-
 if [[ 1 == $SHOULD_HOMEBREW ]]; then
     brew tap homebrew/science
     brew tap homebrew/versions
     brew update
     brew cask install java # required for gradle and more
-    brew install bamutil bash bcftools bedtools blast boost cairo catimg dialog emacs ess fastqc ffmpeg fontconfig fqzcomp freetype gcc gdbm gettext git glib gmp gnutls gradle gsl hdf5 htslib imagemagick isl jpeg lame libevent libffi libmpc libpng libtasn1 libtiff libtool libvo-aacenc mercurial mono mpfr nettle openssl pcre pixman pkg-config poretools pv qemu readline samtools sqlite szip tmux watch wget wxmac wxpython x264 xvid xz
+    brew install bamutil bash bcftools bedtools blast boost cairo catimg dialog emacs ess fastqc ffmpeg fontconfig fqzcomp freetype gcc gdbm gettext git glib gmp gnutls gradle gsl hdf5 htop htslib imagemagick isl jpeg lame libevent libffi libmpc libpng libtasn1 libtiff libtool libvo-aacenc mercurial mono mpfr nettle openssl pcre pixman pkg-config poretools pv qemu readline samtools sqlite szip tmux watch wget wxmac wxpython x264 xvid xz
     brew install docker
-    brew install Caskroom/cask/gpgtools
-    brew install homebrew/dupes/unzip htop-osx
+#    brew install Caskroom/cask/gpgtools
+#    brew install homebrew/dupes/unzip
 fi
 
 # ==============================================================================
@@ -96,11 +93,20 @@ done
 # ==============================================================================
 
 # ==============================================================================
-echo "A bunch of things that should be installed via pip (2 vs 3?)"
+echo "A bunch of things that should be installed via pip2 (Python 2)"
 PIP2_PACKAGES=("awscli", "pylint", "matplotlib", "numpy")
 for PACKAGE in ${PIP2_PACKAGES[@]}; do
-    print "Installing the package $PACKAGE via pip..."
+    print "Installing the package $PACKAGE via pip2..."
     pip2 install $PACKAGE
+done
+# ==============================================================================
+
+# ==============================================================================
+echo "A bunch of things that should be installed via pip3 (Python 3)"
+PIP3_PACKAGES=("awscli", "pylint", "matplotlib", "numpy", "jupyter")
+for PACKAGE in ${PIP3_PACKAGES[@]}; do
+    print "Installing the package $PACKAGE via pip3..."
+    pip3 install $PACKAGE
 done
 # ==============================================================================
 
