2b8a962436f5cef29f4c95c7822d649108efb1e3
Alex SSF <alexgraehl@gmail.com>
2018-06-14 14:54:24 -0700

    Committing any local changes from willia51 before sync with remote server.

--- Lab_Code/Shell/fastq_sort_by_read_name.sh
+++ Lab_Code/Shell/fastq_sort_by_read_name.sh
@@ -4,7 +4,14 @@ set -euo pipefail
 INFQ=$1
 
 OUTFQ=${INFQ}
-OUTFQ=${OUTFQ/.fq/.sorted.fq/}
-OUTFQ=${OUTFQ/.fastq/.sorted.fastq/}
+OUTFQ=${OUTFQ/.fq/.sorted.fq}
+OUTFQ=${OUTFQ/.fastq/.sorted.fastq}
 
-zcat "${INFQ}" | paste - - - - | sort -k1,1g -t \\\" \\\" | tr \\\"\t\\\" \\\"\n\\\" | gzip > "${OUTFQ}"
+if [[ "${INFQ}" == "${OUTFQ}" ]]; then
+    echo "Something went wrong... the input and output filenames are the same???"
+    OUTFQ="${OUTFQ}.sorted.fastq.gz"
+fi
+zcat "${INFQ}" | paste - - - - | sort -k1,1g -t " " | tr "\t" "\n" | gzip > "${OUTFQ}"
+
+(>&2 echo "[STDERR] Generated the output file --> ${OUTFQ}")
+
