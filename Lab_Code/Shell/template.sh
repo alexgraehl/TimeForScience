d2fb6fe9275fa253a0975e25875a27f6f5ae1b28
Alex Williams <alexgraehl@users.noreply.github.com>
2018-10-02 15:58:00 -0700

    Committing any local changes from willia51 before sync with remote server.

--- Lab_Code/Shell/template.sh
+++ Lab_Code/Shell/template.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-set -ueo pipefail
+set -u
+set -o pipefail
+# Don't set -e, which EXITS if any return code is non-zero!
 
-we_need() {
+echoerr() { echo "$@" 1>&2; }
+
+we_need_file() {
     if [[ "$#" != 1 ]] ; then echo -e "[:ERR:]] requires EXACTLY ONE file/directory name!"; exit 1; fi
     if [[ ! -f "$1" ]]; then echo "[:ERR:] Cannot find required file: '$1'"; exit 1; fi
     if [[ ! -s "$1" ]]; then echo "[:ERR:] The file exists, but is EMPTY, which is probably a mistake!: '$1'"; exit 1; fi
@@ -16,5 +20,18 @@ need_executable() {
     return 0
 }
 
-echoerr() { echo "$@" 1>&2; }
+# This does not work
+#is_numeric() {
+#    if [[ $1 =~ ^[+-]?[0-9]+([.][0-9]+)?$ ]] ; then
+#	return 0 # is a number (0 means 'success' in bash)
+#    else
+#	return 1 # not a number (1 means 'not success' in bash)
+#    fi
+#    # Remember that you can't capture the return value with x=$(is_numeric ...): you must check $?
+#}
 
+#if [[ -z $(is_numeric $1) ]]; then
+#    echo "Is numeric"
+#else
+#    echo "Not numeric"
+#fi
