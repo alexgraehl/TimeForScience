9b286834e7529d4d42e2278d50e732d3541a10c9
Alex SSF <alexgraehl@users.noreply.github.com>
2018-10-08 18:15:07 -0700

    Committing any local changes from willia51 before sync with remote server.

--- Lab_Code/Shell/template.sh
+++ Lab_Code/Shell/template.sh
@@ -7,6 +7,7 @@ echoerr() { echo -e "$@" 1>&2; }
 
 
 nonempty_file_exists() { # returns 1, but does not exit with an error
+    # How to use it:   if nonempty_file_exists "myfile" ; then echo "Exists!"; fi
     if [[ "$#" != 1 ]]; then echoerr "[:ERR:]] requires EXACTLY ONE file/directory name!"; exit 1; fi # Bad arguments to the function
     if [[ ! -f "$1" ]]; then echoerr "[:HEY:] File does not exist: '$1'"; return 1; fi
     if [[ ! -s "$1" ]]; then echoerr "[:HEY:] File exists, but is EMPTY: '$1'"; return 1; fi
@@ -15,7 +16,7 @@ nonempty_file_exists() { # returns 1, but does not exit with an error
 
 require_nonempty_file() {
     if [[ "$#" != 1 ]]; then echoerr "[:ERR:]] requires EXACTLY ONE file/directory name!"; exit 1; fi # Bad arguments to the function
-    if [[ ! $(nonempty_file_exists "$1") ]]; then exit 1; fi
+    if ! $(nonempty_file_exists "$1"); then exit 1; fi
     return 0 # 0 is the 'good' status
 }
 
