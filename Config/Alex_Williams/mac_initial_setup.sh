120edda0eb864f5f60ae3cdfd3cee0e8d0c1978c
Alex Williams <alex@healthtensor.com>
2020-07-29 10:06:09 -0700

    Committing any local changes from alexgw before sync with remote server.

--- Config/Alex_Williams/mac_initial_setup.sh
+++ Config/Alex_Williams/mac_initial_setup.sh
@@ -19,8 +19,11 @@ git config --global alias.br   'branch'
 git config --global alias.bl   'branch --list'
 git config --global alias.blr  'branch --list --remote'
 git config --global alias.ci   'commit'
+git config --global alias.s    'status'
 git config --global alias.st   'status'
+git config --global alias.sw   'switch'
 git config --global alias.last 'log -1 HEAD'
+git config --global alias.cot  'checkout --track'  # e.g. git checkout --track origin/remote_branch_name
 
 ###############################################################################
 # General UI/UX                                                               #
