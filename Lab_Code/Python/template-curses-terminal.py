4489d57c0e54a24e208d1a0b870035c2c0537ab8
Alex Williams <alexgraehl@users.noreply.github.com>
2022-01-29 23:45:53 -0800

    Update the CURSES template for python 3.

--- Lab_Code/Python/template-curses-terminal.py
+++ Lab_Code/Python/template-curses-terminal.py
@@ -1,46 +1,30 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 
-'''
-Gene Test
-
-A python program.
-'''
+"""
+A python3 micro-program to show how CURSES (terminal "drawing" output) works. Just shows a grid of colored 'a's.
+"""
 
 import sys
 
-import curses # <-- docs at: http://docs.python.org/library/curses.html
-import curses.ascii   # <-- docs at http://docs.python.org/library/curses.ascii.html
+import curses  # <-- docs at: http://docs.python.org/library/curses.html
+import curses.ascii  # <-- docs at http://docs.python.org/library/curses.ascii.html
 import curses.textpad
-import curses.wrapper
 
-import pdb; #pdb.set_trace() ## Python Debugger! See: http://aymanh.com/python-debugging-techniques
+# import textwrap
+# import getopt
 
-import textwrap
-#import time # we just want "sleep"
-#import os.path
-import getopt
+# import pdb
+# pdb.set_trace() ## Python Debugger! See: http://aymanh.com/python-debugging-techniques
 
-try: 
-    import numpy #from numpy import * # The matrices use this
-except ImportError:
-    print("[TERMINATING EARLY] because the Python module 'numpy' was not installed.")
-    sys.exit(1)
-    pass
 
-class GameGrid:
-    def __init__(self, xsize, ysize):
-        self.xsize = xsize
-        self.ysize = ysize
+# import time # we just want "sleep"
+# import os.path
 
-        self.grid = numpy.zeros(shape=(xsize,ysize), dtype=int)
-        pass
+grid_win = None
 
-gg = GameGrid(xsize=20,ysize=15)
 
-gridWin = None
-
-def agwInitCurses(): # initialize the curses environment                                                                           
-    if (not curses.has_colors()):
+def agwInitCurses():  # initialize the curses environment
+    if not curses.has_colors():
         print("UH OH, this terminal does not support color! Quitting.")
         sys.exit(1)
         pass
@@ -53,47 +37,49 @@ def agwInitCurses(): # initialize the curses environment
     CURSES_VISIBLE_CURSOR = 1
     CURSES_HIGHLIGHTED_CURSOR = 2
     try:
-        curses.curs_set(CURSES_INVISIBLE_CURSOR) # Don't show a blinking cursor                                                  
-    except(curses.error):
-        print("Unable to set cursor state to \"invisible\"")
+        curses.curs_set(CURSES_INVISIBLE_CURSOR)  # Don't show a blinking cursor
+    except (curses.error):
+        print('Unable to set cursor state to "invisible"')
         pass
     curses.meta(1)  # Allow 8-bit chars
     return
 
 
-
-
-def mainScreenHandlingLoop(theScreen):
+def mainScreenHandlingLoop(the_screen):
     agwInitCurses()
-
-    sizeyx = theScreen.getmaxyx()
-    
-    gridWin = curses.newwin(sizeyx[0], sizeyx[1], 0, 0)
-    
+    sizeyx = the_screen.getmaxyx()
+    grid_win = curses.newwin(sizeyx[0], sizeyx[1], 0, 0)
     ch = None
-    while (1):
-        try:
-            theScreen.refresh() # Refresh it first..
 
-            for xxx in range(0, gg.grid.shape[0]):
-                for yyy in range(0, gg.grid.shape[1]):
-                    attr = (curses.color_pair(((xxx+yyy)%2) + 1) + curses.A_NORMAL)
-                    gridWin.attron(attr)
-                    gridWin.addch(yyy, xxx, 'a', attr)
-                    gridWin.attroff(attr)
+    x_size = 80
+    y_size = 20
+    while True:  # <-- infinite loop!
+        try:
+            the_screen.refresh()  # Refresh it first..
+
+            for xxx in range(x_size):
+                for yyy in range(y_size):
+                    attr = (
+                        curses.color_pair(((xxx + yyy) % 2) + 1) + curses.A_NORMAL
+                    )  # Set text color (note the '% 2' modular division to alternate between red and green)
+                    grid_win.attron(attr)
+                    grid_win.addch(yyy, xxx, "a", attr)  # Just add the letter 'a' at this position
+                    grid_win.attroff(attr)
                     pass
                 pass
-            gridWin.refresh()
-            ch = theScreen.getch()    # Now get input from the user...
-            
+            grid_win.addstr("\n")
+            grid_win.addstr("🔥🔥🔥 (Press Ctrl-C to exit) 🔥🔥🔥")
+            grid_win.refresh()
+            ch = the_screen.getch()  # Now get input from the user... (unused in this example code)
+
         except KeyboardInterrupt:
-            break # Exit the program on a Ctrl-C as well. Regular terminal printing is automatically restored by "curses.wrapper"
+            break  # Exit the program on a Ctrl-C as well. Regular terminal printing is automatically restored.
         except:
-            raise # Something unexpected has happened. Better report it!
+            raise  # Something unexpected has happened. Better report it!
 
         pass
-    
-    return # end of mainScreenHandlingLoop
+
+    return  # end of mainScreenHandlingLoop
 
 
 # class KeyDispatcher:
@@ -119,20 +105,12 @@ def mainScreenHandlingLoop(theScreen):
 # d.add_listener([33, 34 35], 'move_up')
 # d.add_listener([33, 34 35], 'move_down')
 
-
 # def main_loop():
 #     while True:
 #         key = get_key()
 #         d.dispatch(key)
 
-
-
 # Must come at the VERY END!
 if __name__ == "__main__":
     curses.wrapper(mainScreenHandlingLoop)
     pass
-
-
-
-
-
