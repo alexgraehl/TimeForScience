854e6c658a963aa7b156c1de5b0027fae57860ac
Alex Williams <alexgraehl@users.noreply.github.com>
2026-08-16 21:53:04 -0700

    disksleep_set_by_time_agw.sh change in description for 2026

--- Lab_Code/Shell/disksleep_set_by_time_agw.sh
+++ Lab_Code/Shell/disksleep_set_by_time_agw.sh
@@ -1,27 +1,32 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # By Alex, 2015.
+#
+# Basically allows spinning (non-SSD) drives to sleep only when you are UNLIKELY to be using the computer.
 # This script checks the current time and decides whether or not to allow disk drives to spin down or not.
-# Useful if you have a lot of external drives; otherwise OS X VERY SLOWLY (10+ seconds) waits for them to spin
-#    up again whenever you trigger an open/save dialog.
+# Useful if you have non-SSD external hard drives; otherwise OS X VERY SLOWLY (10+ seconds) waits for them
+# to spin up again whenever you trigger an open/save dialog.
+#
+# *** Note: must be run as root (use the "setuid" bit) ***
+#
+# FYI: In the Mac OS 26 "Energy" control panel (as of August 2026), the UI-facing name of
+#      this option is "Put hard tidks to sleep when possible"
 
-# Basically allows the drive to sleep only when you are not likely to be using the computer.
-
-# Note: must be run as root (use the "setuid" bit)
-
-# Probably should add it to cron and run it every ***1st minute of every hour.***
+# Recommendation: Probably should add it to cron and run it every ***1st minute of every hour.***
 
 # HOW TO USE:
 # 1) First change the ownership of this script:
-# SCRIPTNAME="this_script" &&  sudo chown root $SCRIPTNAME  &&  sudo chmod 744  $SCRIPTNAME  &&  sudo chmod u+s  $SCRIPTNAME
+#    SCRIPTPATH="this_script" &&  sudo chown root $SCRIPTPATH  &&  sudo chmod 744  $SCRIPTPATH  &&  sudo chmod u+s  $SCRIPTPATH
+#
+#    (I put the script in "/Users/myname/bin/disksleep_set_by_time_agw.sh", so that's what I would use for the SCRIPTPATH.
 
-# 2) Then add it to the ROOT CRONTAB:
-# env EDITOR=nano sudo crontab -e
-# 1 <TAB> * <TAB> * <TAB> * <TAB> * <TAB> /full/path/to/this/script
-# Make sure to use tabs, NOT spaces! There should be four asterisks after the "1".
-# e.g.  1  *  *  *  *  (command path)
-
-# I put the script in /Users/myname/bin/disksleep_set_by_time_agw.sh
+# 2) Then edit the ROOT CRONTAB to run this script periodically:
+#     env EDITOR=nano sudo crontab -e
+#     17<TAB>*<TAB>*<TAB>*<TAB>*<TAB>/full/path/to/this/script
+#     Make sure to use tabs, NOT spaces! There should be four asterisks after the "17".
+#     Visually, the result will look like this:
+#     17   *    *    *    *    (command path)
+#     Again, you must use TABS and not spaces.
 
 NOW=$(date "+%u-%H-%M") # DAY_OF_WEEK:HOUR24:MINUTE60  (e.g 1:18:22 or 8:18:22)
 # %u: day of week (1..7); 1 represents Monday, 6 is Saturday, 7 is Sunday
@@ -45,11 +50,6 @@ WEEKEND_END_SLEEP=10  # 10 AM. MUST BE GREATER THAN "START"
 
 WORKDAY_START_SLEEP=3 # 3 AM. MUST BE AFTER MIDNIGHT OR THE COMPARISONS WON'T WORK.
 WORKDAY_END_SLEEP=17  # 5 PM. MUST BE GREATER THAN "START"
-
-#echo $ISWEEKEND
-#echo $WEEKDAY
-#echo $HR
-#echo $MIN
 
 # pmset -c means only set it for when we are CONNECTED TO WALL POWER (-c). Does not affect disk spin when on battery.
 
